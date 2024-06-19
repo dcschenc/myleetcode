8f51039233b791ea3d3b4b2e686343839e202620
dcschenc <dcschenc@gmail.com>
2024-06-19 20:09:34 +0800

    Time: 448 ms (88.61%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+# select w1.id from Weather w1, Weather w2 where w1.id = (w2.id + 1) and w1.temperature > w2.temperature
+select w1.id from Weather w1, Weather w2 
+where datediff(w1.recordDate, w2.recordDate) = 1 and w1.temperature > w2.temperature
