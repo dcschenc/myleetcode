ae8178440b02d17c2c0e1894b20da710ad9a6f1e
dcschenc <dcschenc@gmail.com>
2024-06-20 19:26:37 +0800

    Time: 55 ms (74.48%), Space: 18.3 MB (90.9%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        # difference between two numbers (x and y) which were seen only once
+        bitmask = 0
+        for num in nums:
+            bitmask ^= num
+        
+        # rightmost 1-bit diff between x and y
+        diff = bitmask & (-bitmask)
+        
+        x = 0
+        for num in nums:
+            # bitmask which will contain only x
+            if num & diff:
+                x ^= num
+        
+        return [x, bitmask^x]
+    
+        hashmap = Counter(nums)
+        return [x for x in hashmap if hashmap[x] == 1]
