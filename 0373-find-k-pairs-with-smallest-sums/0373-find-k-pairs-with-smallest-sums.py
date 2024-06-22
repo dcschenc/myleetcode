ebafecc8547cf065083d9bba3c88a7e58741f51f
dcschenc <dcschenc@gmail.com>
2024-06-22 09:22:12 +0800

    Time: 966 ms (72.05%), Space: 36 MB (77.27%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
@@ -1,28 +1,30 @@
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        if not nums1 or not nums2:
-            return []
+        # https://leetcode.com/problems/find-k-pairs-with-smallest-sums/editorial/
+        m = len(nums1)
+        n = len(nums2)
 
-        heap = []  # Create a min-heap to store pairs (a, b)
+        ans = []
+        visited = set()
 
-        # Initialize the heap with the first k pairs (nums1[0], nums2[0..k-1])
-        for i in range(min(k, len(nums2))):
-            heapq.heappush(heap, (nums1[0] + nums2[i], 0, i))
+        minHeap = [(nums1[0] + nums2[0], (0, 0))]
+        visited.add((0, 0))
+        count = 0
 
-        result = []
+        while k > 0 and minHeap:
+            val, (i, j) = heappop(minHeap)
+            ans.append([nums1[i], nums2[j]])
+
+            if i + 1 < m and (i + 1, j) not in visited:
+                heappush(minHeap, (nums1[i + 1] + nums2[j], (i + 1, j)))
+                visited.add((i + 1, j))
+
+            if j + 1 < n and (i, j + 1) not in visited:
+                heappush(minHeap, (nums1[i] + nums2[j + 1], (i, j + 1)))
+                visited.add((i, j + 1))
+            k = k - 1
         
-        while k > 0 and heap:
-            sum_ab, i, j = heapq.heappop(heap)
-            result.append([nums1[i], nums2[j]])
-
-            # If there are more pairs in nums1, add the next pair (nums1[i+1], nums2[j]) to the heap
-            if i + 1 < len(nums1):
-                heapq.heappush(heap, (nums1[i + 1] + nums2[j], i + 1, j))
-            
-            k -= 1
-
-        return result
-
+        return ans
         # m, n = len(nums1), len(nums2)
         # mat = [[0] * n for i in range(m)]
         # for i in range(m):
