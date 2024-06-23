4a7b992e140ded45627c7e46c54a090ad44859d8
dcschenc <dcschenc@gmail.com>
2024-06-23 15:24:31 +0800

    Time: 34 ms (78.46%), Space: 17.1 MB (36.71%) - LeetHub

---
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.py
@@ -0,0 +1,75 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':        
+        if head is None:
+            return head
+        node = head
+        self.get_childs(node)
+        return head
+        
+    
+    def get_childs(self, node):
+        prev = None
+        while node:
+            if node.child is None:
+                prev = node
+                node = node.next
+            else:
+                right = node.next
+                node.next = node.child
+                node.child.prev = node
+                last_child = self.get_childs(node.child)
+                last_child.next = right
+                if right:
+                    right.prev = last_child
+                node.child = None
+                node = right
+                prev = last_child
+        return prev
+
+        # if not head:
+        #     return None
+
+        # dummy = Node(0)
+        # dummy.next = head
+        # stack = [head]
+        # prev = dummy
+
+        # while stack:
+        #     cur = stack.pop()
+
+        #     if cur.next:
+        #         stack.append(cur.next)
+
+        #     if cur.child:
+        #         stack.append(cur.child)
+        #         cur.child = None
+
+        #     prev.next = cur
+        #     cur.prev = prev
+        #     prev = cur
+
+        # dummy.next.prev = None
+        # return dummy.next
+        
+        
+        # child = node.child
+        # right = node.next
+        # node.next = child
+        # child.prev = node
+        # if child.child:
+        #     child_last = get_childs(child)
+        #     child_last.next = right
+        # else:
+        #     child = child.next
+        
+        
