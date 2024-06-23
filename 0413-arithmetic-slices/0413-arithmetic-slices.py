158532a218fa6ecfa4429c3135d13a57bbdefee5
dcschenc <dcschenc@gmail.com>
2024-06-23 13:52:50 +0800

    Time: 46 ms (17.45%), Space: 16.8 MB (64.64%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.py
+++ 0413-arithmetic-slices/0413-arithmetic-slices.py
@@ -5,11 +5,8 @@ class Solution:
         left, total = 0, 0
         for right in range(2, n):
             if nums[right] - nums[right - 1] == nums[right - 1] - nums[right - 2]:
-                # total += (right - left) + 1 if right - left >= 2 else 1
-                if right - left == 2:
-                    total += 1
-                else:
-                    total += (right - left - 1)
+
+                total += (right - left - 1)
             else:
                 left = right - 1
         return total
