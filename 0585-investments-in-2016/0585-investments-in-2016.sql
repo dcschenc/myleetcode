26e5b36e49f9329ddc8ce087d08034d029451fad
dcschenc <dcschenc@gmail.com>
2024-06-26 17:51:32 +0800

    Time: 436 ms (90.42%), Space: 0B (100%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+with t as(
+    select  tiv_2016, 
+            count(pid) over(partition by tiv_2015) as cnt1,
+            count(pid) over(partition by lat, lon) as cnt2
+    from Insurance
+)
+
+select round(sum(tiv_2016), 2) as tiv_2016
+from t
+where cnt1 > 1 and cnt2 = 1
