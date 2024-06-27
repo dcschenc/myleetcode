ccb893bb68c9201163549d64f0e1ca4d6e2eee1f
dcschenc <dcschenc@gmail.com>
2024-06-27 19:37:06 +0800

    Time: 436 ms (63.54%), Space: 19.2 MB (34.9%) - LeetHub

---
+++ 0757-pyramid-transition-matrix/0757-pyramid-transition-matrix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
+        @cache
+        def dfs(s):
+            if len(s) == 1:
+                return True
+            t = []
+            for a, b in pairwise(s):
+                cs = d[a, b]
+                if not cs:
+                    return False
+                t.append(cs)
+            print(t)
+            for nxt in product(*t):
+                print(nxt)
+            return any(dfs(''.join(nxt)) for nxt in product(*t))
+
+        d = defaultdict(list)
+        for a, b, c in allowed:
+            d[a, b].append(c)
+        return dfs(bottom)
