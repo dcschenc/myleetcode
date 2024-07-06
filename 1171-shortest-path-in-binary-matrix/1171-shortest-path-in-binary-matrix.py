6512381482c561d17b300c25c3cc824e88ab8769
dcschenc <dcschenc@gmail.com>
2024-07-06 15:18:38 +0800

    Time: 516 ms (27.75%), Space: 18.3 MB (13.17%) - LeetHub

---
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -0,0 +1,42 @@
+from collections import deque
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        queue = deque()
+        if grid[0][0] != 0:
+            return -1
+        queue.append((0, 0, 1))
+        visited = set()
+        dr = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+        while queue:
+            for i in range(len(queue)):
+                x, y, length = queue.popleft()
+                if x == m-1 and y == n-1:
+                    return length
+                if (x, y) in visited:
+                    continue
+                visited.add((x, y))
+                for dx, dy in dr:
+                    newX, newY = dx + x, dy + y
+                    if 0 <= newX <= m-1 and 0<= newY <= n-1 and grid[newX][newY] == 0:
+                        queue.append((newX, newY, length + 1))
+        return -1
+
+        if grid[0][0]:
+            return -1
+        n = len(grid)
+        grid[0][0] = 1
+        q = deque([(0, 0)])
+        ans = 1
+        while q:
+            for _ in range(len(q)):
+                i, j = q.popleft()
+                if i == j == n - 1:
+                    return ans
+                for x in range(i - 1, i + 2):
+                    for y in range(j - 1, j + 2):
+                        if 0 <= x < n and 0 <= y < n and grid[x][y] == 0:
+                            grid[x][y] = 1
+                            q.append((x, y))
+            ans += 1
+        return -1
