ef4fe838dbf226ae2deea924f9486839d8060dbf
dcschenc <dcschenc@gmail.com>
2024-07-07 15:32:31 +0800

    Time: 62 ms (11.85%), Space: 17 MB (63.76%) - LeetHub

--- 1196-filling-bookcase-shelves/1196-filling-bookcase-shelves.py
+++ 1196-filling-bookcase-shelves/1196-filling-bookcase-shelves.py
@@ -1,19 +1,20 @@
 class Solution:
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
-            n = len(books)
-            dp = [float('inf')] * (n + 1)
-            dp[0] = 0
-            for i in range(1, n + 1):
-                width = 0
-                max_height = 0
-                j = i
-                # for each previou `book[j]`, verify if it can be placed in the same row as `book[i]`
-                while j > 0:   
-                    width += books[j - 1][0]  # Width of the current book
-                    if width > shelfWidth:
-                        break
-                    max_height = max(max_height, books[j - 1][1])  # Height of the current book
-                    dp[i] = min(dp[i], dp[j - 1] + max_height)
-                    j -= 1
+        # https://github.com/doocs/leetcode/tree/main/solution/1100-1199/1105.Filling%20Bookcase%20Shelves
+        n = len(books)
+        dp = [float('inf')] * (n + 1)
+        dp[0] = 0
+        for i in range(1, n + 1):
+            width = 0
+            max_height = 0
+            j = i
+            # for each previou `book[j]`, verify if it can be placed in the same row as `book[i]`
+            while j > 0:   
+                width += books[j - 1][0]  # Width of the current book
+                if width > shelfWidth:
+                    break
+                max_height = max(max_height, books[j - 1][1])  # Height of the current book
+                dp[i] = min(dp[i], dp[j - 1] + max_height)
+                j -= 1
 
-            return dp[n]
+        return dp[n]
