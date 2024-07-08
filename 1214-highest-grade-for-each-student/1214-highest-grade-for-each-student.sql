69166da00ab407376ce827907bd6e0fe17faca91
dcschenc <dcschenc@gmail.com>
2024-07-08 19:58:11 +0800

    Time: 431 ms (95.24%), Space: 0B (100%) - LeetHub

---
+++ 1214-highest-grade-for-each-student/1214-highest-grade-for-each-student.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select student_id, course_id, grade from (
+    select *, rank() over(partition by student_id order by grade desc, course_id) as rnk
+    from Enrollments
+) a where rnk = 1 order by student_id
