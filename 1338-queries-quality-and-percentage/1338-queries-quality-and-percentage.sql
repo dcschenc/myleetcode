83b4f87af7043c92dd0139cb36325091c8ffbc12
dcschenc <dcschenc@gmail.com>
2024-07-10 19:45:48 +0800

    Time: 347 ms (68.7%), Space: 0B (100%) - LeetHub

---
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select query_name, 
+       round(avg(rating/position), 2) as quality, 
+       round(sum(rating < 3) * 100 / count(*), 2) as poor_query_percentage 
+from Queries 
+where query_name is not null
+group by query_name
