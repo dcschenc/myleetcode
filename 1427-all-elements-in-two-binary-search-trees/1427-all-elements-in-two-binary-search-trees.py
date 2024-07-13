23489ba4f975691827fe293ea95913e9b6ce4e84
dcschenc <dcschenc@gmail.com>
2024-07-13 14:15:43 +0800

    Time: 175 ms (71.98%), Space: 19.6 MB (90.99%) - LeetHub

--- 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.py
+++ 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.py
@@ -6,6 +6,32 @@
 #         self.right = right
 class Solution:
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        stack1, stack2, output = [], [], []
+        
+        while root1 or root2 or stack1 or stack2:
+            # update both stacks
+            # by going left till possible
+            while root1:
+                stack1.append(root1)
+                root1 = root1.left
+            while root2:
+                stack2.append(root2)
+                root2 = root2.left
+
+            # Add the smallest value into output,
+            # pop it from the stack,
+            # and then do one step right
+            if not stack2 or stack1 and stack1[-1].val <= stack2[-1].val:
+                root1 = stack1.pop()
+                output.append(root1.val)
+                root1 = root1.right
+            else:
+                root2 = stack2.pop()
+                output.append(root2.val)   
+                root2 = root2.right
+
+        return output
+        
         def dfs(node, l):
             if not node:
                 return
