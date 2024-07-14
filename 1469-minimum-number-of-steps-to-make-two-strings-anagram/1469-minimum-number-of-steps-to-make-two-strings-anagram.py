f2f0332120fe14920ac7b5743ae8608ced61bdec
dcschenc <dcschenc@gmail.com>
2024-07-14 09:51:04 +0800

    Time: 203 ms (16.92%), Space: 17.2 MB (24.01%) - LeetHub

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,5 +1,15 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/1300-1399/1347.Minimum%20Number%20of%20Steps%20to%20Make%20Two%20Strings%20Anagram
+        cnt = Counter(s)
+        ans = 0
+        for c in t:
+            if cnt[c] > 0:
+                cnt[c] -= 1
+            else:
+                ans += 1
+        return ans
+
         counter1, counter2 = Counter(s), Counter(t)
         n = len(s)
         common = 0
