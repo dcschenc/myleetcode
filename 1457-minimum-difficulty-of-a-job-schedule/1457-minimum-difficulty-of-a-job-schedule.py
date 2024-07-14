77fa0f4e3becee38f2971c3362c9e21ee63c8b2f
dcschenc <dcschenc@gmail.com>
2024-07-14 09:36:35 +0800

    Time: 653 ms (38.18%), Space: 16.8 MB (76.8%) - LeetHub

--- 1457-minimum-difficulty-of-a-job-schedule/1457-minimum-difficulty-of-a-job-schedule.py
+++ 1457-minimum-difficulty-of-a-job-schedule/1457-minimum-difficulty-of-a-job-schedule.py
@@ -5,7 +5,7 @@ class Solution:
         f = [[inf] * (d + 1) for _ in range(n + 1)]
         f[0][0] = 0
         for i in range(1, n + 1):
-            for j in range(1, min(d + 1, i + 1)):
+            for j in range(1, d + 1):
                 mx = 0
                 for k in range(i, 0, -1):
                     mx = max(mx, jobDifficulty[k - 1])
