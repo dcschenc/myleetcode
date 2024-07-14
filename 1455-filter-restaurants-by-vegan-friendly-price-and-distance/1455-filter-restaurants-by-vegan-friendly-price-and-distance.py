536d4d22c1b0c9754245a4111fe9ee4a37a3b10b
dcschenc <dcschenc@gmail.com>
2024-07-14 09:20:00 +0800

    Time: 280 ms (47.46%), Space: 25.3 MB (7.91%) - LeetHub

--- 1455-filter-restaurants-by-vegan-friendly-price-and-distance/1455-filter-restaurants-by-vegan-friendly-price-and-distance.py
+++ 1455-filter-restaurants-by-vegan-friendly-price-and-distance/1455-filter-restaurants-by-vegan-friendly-price-and-distance.py
@@ -3,6 +3,7 @@ class Solution:
         # hm = defaultdict(list)
         # for rid, r, v, p, d in restaurants:
         #     hm[v].append((p, d, r, rid))
+        # https://github.com/doocs/leetcode/tree/main/solution/1300-1399/1333.Filter%20Restaurants%20by%20Vegan-Friendly%2C%20Price%20and%20Distance
         ans = []
         for rid, r, v, p, d in restaurants:
             if veganFriendly and v == 0:
