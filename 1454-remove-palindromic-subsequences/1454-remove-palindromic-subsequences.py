dcec9f850348b93d7f500a929c7625374c691077
dcschenc <dcschenc@gmail.com>
2024-07-14 09:16:22 +0800

    Time: 37 ms (43.58%), Space: 16.6 MB (12.43%) - LeetHub

--- 1454-remove-palindromic-subsequences/1454-remove-palindromic-subsequences.py
+++ 1454-remove-palindromic-subsequences/1454-remove-palindromic-subsequences.py
@@ -1,5 +1,8 @@
 class Solution:
     def removePalindromeSub(self, s: str) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/1300-1399/1332.Remove%20Palindromic%20Subsequences
+        return 1 if s == s[::-1] else 2
+        
         if len(s) == 0:
             return 0
         left, right = 0, len(s) - 1
