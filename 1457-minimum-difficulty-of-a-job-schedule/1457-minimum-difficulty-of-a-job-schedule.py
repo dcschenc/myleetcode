c7ebb68bbb8db1c393f5b74d5aaf233c0f064810
dcschenc <dcschenc@gmail.com>
2024-07-14 09:34:24 +0800

    Time: 642 ms (39.98%), Space: 16.8 MB (76.8%) - LeetHub

---
+++ 1457-minimum-difficulty-of-a-job-schedule/1457-minimum-difficulty-of-a-job-schedule.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/1300-1399/1335.Minimum%20Difficulty%20of%20a%20Job%20Schedule
+        n = len(jobDifficulty)
+        f = [[inf] * (d + 1) for _ in range(n + 1)]
+        f[0][0] = 0
+        for i in range(1, n + 1):
+            for j in range(1, min(d + 1, i + 1)):
+                mx = 0
+                for k in range(i, 0, -1):
+                    mx = max(mx, jobDifficulty[k - 1])
+                    f[i][j] = min(f[i][j], f[k - 1][j - 1] + mx)
+        return -1 if f[n][d] >= inf else f[n][d]        
