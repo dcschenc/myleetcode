52b99eb475ae96e09788538cd96f925d4fe853a2
dcschenc <dcschenc@gmail.com>
2024-07-16 19:44:57 +0800

    Time: 120 ms (86.46%), Space: 31 MB (75.81%) - LeetHub

---
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/1400-1499/1448.Count%20Good%20Nodes%20in%20Binary%20Tree
+        def dfs(node: TreeNode, mx: int):
+            if node is None: return
+            nonlocal ans
+            if mx <= node.val:
+                ans += 1
+                mx = node.val
+            dfs(node.left, mx)
+            dfs(node.right, mx)
+
+        ans = 0
+        dfs(root, -1000000)
+        return ans
+
+        def dfs(node, max_val):
+            if not node:
+                return 0
+            cnt = 0
+            if node.val >= max_val:
+                # res.append(node)
+                cnt = 1
+                max_val = node.val
+            # if node.left:
+            cnt_left = dfs(node.left, max_val)
+            # if node.right:
+            cnt_right = dfs(node.right, max_val)
+            return cnt_left + cnt_right + cnt
+            # return cnt
+        
+        node = root
+        # res = []
+        cnt = dfs(node, -float('inf'))
+        return cnt
+        # return len(res)
