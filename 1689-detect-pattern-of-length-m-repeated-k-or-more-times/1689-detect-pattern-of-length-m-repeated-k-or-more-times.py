be6192dd59e5d38c4777388aa0416358a15cdcfd
dcschenc <dcschenc@gmail.com>
2024-07-20 09:20:38 +0800

    Time: 28 ms (99.63%), Space: 16.5 MB (28.25%) - LeetHub

---
+++ 1689-detect-pattern-of-length-m-repeated-k-or-more-times/1689-detect-pattern-of-length-m-repeated-k-or-more-times.py
@@ -0,0 +1,19 @@
+class Solution:
+    def containsPattern(self, arr: List[int], m: int, k: int) -> bool:
+        # hm = collections.defaultdict(int)
+        # for i in range(m, len(arr)+1):
+        #     key = tuple(arr[i-m:i])
+        #     # print(key, hm)
+        #     hm[key] += 1
+        #     if hm[key] == k:
+        #         return True
+        # return False
+        n = len(arr) 
+        for i in range(n - m*k + 1):
+           
+                if arr[i:i+m] * k == arr[i:i+m*k]:
+                    # print(i, m, arr[i:m])
+                    return True
+          
+        return False
+
