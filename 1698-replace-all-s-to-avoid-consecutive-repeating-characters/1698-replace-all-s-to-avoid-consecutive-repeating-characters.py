e91ebc878d1f37515323f59d8ed436f40c48e49c
dcschenc <dcschenc@gmail.com>
2024-07-20 09:46:13 +0800

    Time: 33 ms (78.63%), Space: 16.4 MB (98.01%) - LeetHub

--- 1698-replace-all-s-to-avoid-consecutive-repeating-characters/1698-replace-all-s-to-avoid-consecutive-repeating-characters.py
+++ 1698-replace-all-s-to-avoid-consecutive-repeating-characters/1698-replace-all-s-to-avoid-consecutive-repeating-characters.py
@@ -1,5 +1,16 @@
 class Solution:
     def modifyString(self, s: str) -> str:
+        s = list(s)
+        n = len(s)
+        for i in range(n):
+            if s[i] == "?":
+                for c in "abc":
+                    if (i and s[i - 1] == c) or (i + 1 < n and s[i + 1] == c):
+                        continue
+                    s[i] = c
+                    break
+        return "".join(s)
+        
         ans = ''
         for i, c in enumerate(s):
             if c != '?':
