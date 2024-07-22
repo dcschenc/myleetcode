f6d83122e3befdf9ab8ed18f6101796015a0528b
dcschenc <dcschenc@gmail.com>
2024-07-22 19:38:07 +0800

    Time: 3397 ms (20%), Space: 91.8 MB (47.5%) - LeetHub

---
+++ 1822-longest-palindromic-subsequence-ii/1822-longest-palindromic-subsequence-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/1600-1699/1682.Longest%20Palindromic%20Subsequence%20II
+        @cache
+        def dfs(i, j, x):
+            if i >= j:
+                return 0
+            if s[i] == s[j] and s[i] != x:
+                return dfs(i + 1, j - 1, s[i]) + 2
+            return max(dfs(i + 1, j, x), dfs(i, j - 1, x))
+
+        ans = dfs(0, len(s) - 1, '')
+        dfs.cache_clear()
+        return ans
