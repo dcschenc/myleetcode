dbbc5c102ba23af5d3903e1baeed7e74f9d8a58c
dcschenc <dcschenc@gmail.com>
2024-07-27 11:28:01 +0800

    Time: 129 ms (86.08%), Space: 16.1 MB (100%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
@@ -1,5 +1,6 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/1900-1999/1930.Unique%20Length-3%20Palindromic%20Subsequences
         ans = 0
         for c in ascii_lowercase:
             l, r = s.find(c), s.rfind(c)
@@ -7,22 +8,22 @@ class Solution:
                 ans += len(set(s[l + 1 : r]))
         return ans
         
-        pairs = set()
-        n = len(s)
-        pre = [set()] * n
-        for i, c in enumerate(s):
-            if i > 0:
-                pre[i] = pre[i-1].copy()
-            pre[i].add(c)
+        # pairs = set()
+        # n = len(s)
+        # pre = [set()] * n
+        # for i, c in enumerate(s):
+        #     if i > 0:
+        #         pre[i] = pre[i-1].copy()
+        #     pre[i].add(c)
         
-        post = [set()] * n
-        for i in range(n-1, -1, -1):
-            if i < n - 1:
-                post[i] = post[i + 1].copy()
-            post[i].add(s[i])        
+        # post = [set()] * n
+        # for i in range(n-1, -1, -1):
+        #     if i < n - 1:
+        #         post[i] = post[i + 1].copy()
+        #     post[i].add(s[i])        
 
-        for i in range(1, n-1):
-            for c in pre[i-1]:
-                if c in post[i+1]:
-                    pairs.add((c, s[i], c))        
-        return len(pairs)
+        # for i in range(1, n-1):
+        #     for c in pre[i-1]:
+        #         if c in post[i+1]:
+        #             pairs.add((c, s[i], c))        
+        # return len(pairs)
