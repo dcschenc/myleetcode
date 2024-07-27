a9c877f0dc1f1c017fdc22e19aaa7db96a1243fa
dcschenc <dcschenc@gmail.com>
2024-07-27 09:52:05 +0800

    Time: 1371 ms (60.74%), Space: 17.6 MB (34.23%) - LeetHub

--- 2044-number-of-wonderful-substrings/2044-number-of-wonderful-substrings.py
+++ 2044-number-of-wonderful-substrings/2044-number-of-wonderful-substrings.py
@@ -1,6 +1,28 @@
 class Solution:
     def wonderfulSubstrings(self, word: str) -> int:
-    # https://github.com/doocs/leetcode/tree/main/solution/1900-1999/1915.Number%20of%20Wonderful%20Substrings        
+        # https://github.com/doocs/leetcode/tree/main/solution/1900-1999/1915.Number%20of%20Wonderful%20Substrings        
+        count = defaultdict(int)
+        count[0] = 1  # Start with the initial mask
+        current_mask = 0
+        wonderful_count = 0
+        
+        for char in word:
+            # Flip the bit corresponding to the current character
+            current_mask ^= (1 << (ord(char) - ord('a')))
+            
+            # Count substrings ending at this position with the current_mask
+            wonderful_count += count[current_mask]
+            
+            # Check for masks differing by exactly one bit
+            for i in range(10):
+                test_mask = current_mask ^ (1 << i)
+                wonderful_count += count[test_mask]
+            
+            # Update the count of the current mask
+            count[current_mask] += 1
+        
+        return wonderful_count
+        
         # Create the frequency map
         # Key = bitmask, Value = frequency of bitmask key
         freq = {}
