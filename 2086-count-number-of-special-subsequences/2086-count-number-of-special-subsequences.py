7befb23007382f9d006f7d81091ee3141a71b0e2
dcschenc <dcschenc@gmail.com>
2024-07-27 15:08:41 +0800

    Time: 1840 ms (38.27%), Space: 33 MB (13.58%) - LeetHub

---
+++ 2086-count-number-of-special-subsequences/2086-count-number-of-special-subsequences.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countSpecialSubsequences(self, nums: List[int]) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/1900-1999/1955.Count%20Number%20of%20Special%20Subsequences
+        mod = 10**9 + 7
+        n = len(nums)
+        f = [[0] * 3 for _ in range(n)]
+        f[0][0] = nums[0] == 0
+        for i in range(1, n):
+            if nums[i] == 0:
+                f[i][0] = (2 * f[i - 1][0] + 1) % mod
+                f[i][1] = f[i - 1][1]
+                f[i][2] = f[i - 1][2]
+            elif nums[i] == 1:
+                f[i][0] = f[i - 1][0]
+                f[i][1] = (f[i - 1][0] + 2 * f[i - 1][1]) % mod
+                f[i][2] = f[i - 1][2]
+            else:
+                f[i][0] = f[i - 1][0]
+                f[i][1] = f[i - 1][1]
+                f[i][2] = (f[i - 1][1] + 2 * f[i - 1][2]) % mod
+        return f[n - 1][2]
