2a9a59d07eb691a528d439e73c89b5de6c7249fa
dcschenc <dcschenc@gmail.com>
2024-07-28 19:42:15 +0800

    Time: 1588 ms (11.02%), Space: 0B (100%) - LeetHub

---
+++ 2167-number-of-accounts-that-did-not-stream/2167-number-of-accounts-that-did-not-stream.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select count(account_id) as accounts_count from Subscriptions 
+where year(end_date) = 2021 and account_id not in (
+    select account_id from Streams where year(stream_date) = 2021
+)
