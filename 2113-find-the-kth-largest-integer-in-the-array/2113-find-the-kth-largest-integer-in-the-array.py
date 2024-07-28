9127f98293856ed5b4e4fae836edf772cc4a8596
dcschenc <dcschenc@gmail.com>
2024-07-28 11:12:38 +0800

    Time: 277 ms (14.66%), Space: 25.2 MB (15.28%) - LeetHub

--- 2113-find-the-kth-largest-integer-in-the-array/2113-find-the-kth-largest-integer-in-the-array.py
+++ 2113-find-the-kth-largest-integer-in-the-array/2113-find-the-kth-largest-integer-in-the-array.py
@@ -1,16 +1,16 @@
 class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
-        # heaps = []
-        # for num in nums:
-        #     heappush(heaps, int(num))
-        #     while len(heaps) > k:
-        #         heappop(heaps)
-        # return str(heaps[0])
+        heaps = []
+        for num in nums:
+            heappush(heaps, int(num))
+            while len(heaps) > k:
+                heappop(heaps)
+        return str(heaps[0])
 
-        def cmp(a, b):
-            if len(a) != len(b):
-                return len(b) - len(a)
-            return 1 if b > a else -1
+        # def cmp(a, b):
+        #     if len(a) != len(b):
+        #         return len(b) - len(a)
+        #     return 1 if b > a else -1
 
-        nums.sort(key=cmp_to_key(cmp))
-        return nums[k - 1]    
+        # nums.sort(key=cmp_to_key(cmp))
+        # return nums[k - 1]    
