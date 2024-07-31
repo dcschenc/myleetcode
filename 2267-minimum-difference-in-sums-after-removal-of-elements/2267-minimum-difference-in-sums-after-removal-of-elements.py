31f00b21aa1c970fbcd31be639e5905d28ab6390
dcschenc <dcschenc@gmail.com>
2024-07-31 20:00:31 +0800

    Time: 1362 ms (44.44%), Space: 49.8 MB (7.94%) - LeetHub

---
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minimumDifference(self, nums: List[int]) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/2100-2199/2163.Minimum%20Difference%20in%20Sums%20After%20Removal%20of%20Elements
+        m = len(nums)
+        n = m // 3
+
+        s = 0
+        pre = [0] * (m + 1)
+        q1 = []
+        for i, x in enumerate(nums[: n * 2], 1):
+            s += x
+            heappush(q1, -x)
+            if len(q1) > n:
+                s -= -heappop(q1)
+            pre[i] = s
+
+        s = 0
+        suf = [0] * (m + 1)
+        q2 = []
+        for i in range(m, n, -1):
+            x = nums[i - 1]
+            s += x
+            heappush(q2, x)
+            if len(q2) > n:
+                s -= heappop(q2)
+            suf[i] = s
+
+        return min(pre[i] - suf[i + 1] for i in range(n, n * 2 + 1))
