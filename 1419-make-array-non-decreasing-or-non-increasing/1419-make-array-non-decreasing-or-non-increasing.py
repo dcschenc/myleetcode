9998abbdee0bd4e974d215a57dbe025ca183b6db
dcschenc <dcschenc@gmail.com>
2024-08-02 19:50:14 +0800

    Time: 2596 ms (10.7%), Space: 55.6 MB (5.2%) - LeetHub

---
+++ 1419-make-array-non-decreasing-or-non-increasing/1419-make-array-non-decreasing-or-non-increasing.py
@@ -0,0 +1,15 @@
+class Solution:
+    def convertArray(self, nums: List[int]) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/2200-2299/2263.Make%20Array%20Non-decreasing%20or%20Non-increasing
+        def solve(nums):
+            n = len(nums)
+            f = [[0] * 1001 for _ in range(n + 1)]
+            for i, x in enumerate(nums, 1):
+                mi = inf
+                for j in range(1001):
+                    if mi > f[i - 1][j]:
+                        mi = f[i - 1][j]
+                    f[i][j] = mi + abs(x - j)
+            return min(f[n])
+
+        return min(solve(nums), solve(nums[::-1]))
