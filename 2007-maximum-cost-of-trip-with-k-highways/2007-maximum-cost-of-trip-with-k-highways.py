de5017b443ad6f4031ceb10abab0c4005d846fb3
dcschenc <dcschenc@gmail.com>
2024-08-02 19:47:31 +0800

    Time: 429 ms (44.12%), Space: 23.9 MB (76.47%) - LeetHub

---
+++ 2007-maximum-cost-of-trip-with-k-highways/2007-maximum-cost-of-trip-with-k-highways.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumCost(self, n: int, highways: List[List[int]], k: int) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/2200-2299/2247.Maximum%20Cost%20of%20Trip%20With%20K%20Highways
+        if k >= n:
+            return -1
+        g = defaultdict(list)
+        for a, b, cost in highways:
+            g[a].append((b, cost))
+            g[b].append((a, cost))
+        f = [[-inf] * n for _ in range(1 << n)]
+        for i in range(n):
+            f[1 << i][i] = 0
+        ans = -1
+        for i in range(1 << n):
+            for j in range(n):
+                if i >> j & 1:
+                    for h, cost in g[j]:
+                        if i >> h & 1:
+                            f[i][j] = max(f[i][j], f[i ^ (1 << j)][h] + cost)
+                if i.bit_count() == k + 1:
+                    ans = max(ans, f[i][j])
+        return ans
