1be5ac6fefd97622dbbf70ca65a7087d020bf058
dcschenc <dcschenc@gmail.com>
2024-08-03 14:40:27 +0800

    Time: 370 ms (36.51%), Space: 30.8 MB (48.43%) - LeetHub

--- 2474-minimum-adjacent-swaps-to-make-a-valid-array/2474-minimum-adjacent-swaps-to-make-a-valid-array.py
+++ 2474-minimum-adjacent-swaps-to-make-a-valid-array/2474-minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -1,5 +1,6 @@
 class Solution:
     def minimumSwaps(self, nums: List[int]) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/2300-2399/2340.Minimum%20Adjacent%20Swaps%20to%20Make%20a%20Valid%20Array
         mx, mx_idx, mi, mi_idx = 0, 0, float('inf'), 0
         n, ans = len(nums), 0
         for i in range(n):
