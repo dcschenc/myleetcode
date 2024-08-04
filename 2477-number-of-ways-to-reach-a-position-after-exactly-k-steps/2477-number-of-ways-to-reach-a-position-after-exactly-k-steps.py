fd38e774b7c803d4dcc85ccbb5ba2a2409f52813
dcschenc <dcschenc@gmail.com>
2024-08-04 15:14:29 +0800

    Time: 1910 ms (19.59%), Space: 699.9 MB (17.14%) - LeetHub

--- 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/2477-number-of-ways-to-reach-a-position-after-exactly-k-steps.py
+++ 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/2477-number-of-ways-to-reach-a-position-after-exactly-k-steps.py
@@ -9,8 +9,8 @@ class Solution:
                     return 1
                 return 0
             ans = 0
-            ans += dfs(i + 1, j + 1)
-            ans += dfs(i - 1, j + 1)
+            ans += dfs(i + 1, j + 1) % mod
+            ans += dfs(i - 1, j + 1) % mod
             return ans
         mod = 10 ** 9 + 7
         return dfs(startPos, 0) % mod
