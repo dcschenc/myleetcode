06373693ff0560d2bc6413fa307f926409daa4ce
dcschenc <dcschenc@gmail.com>
2024-08-04 14:17:39 +0800

    Time: 756 ms (19.78%), Space: 49.8 MB (13.19%) - LeetHub

--- 2522-median-of-a-row-wise-sorted-matrix/2522-median-of-a-row-wise-sorted-matrix.py
+++ 2522-median-of-a-row-wise-sorted-matrix/2522-median-of-a-row-wise-sorted-matrix.py
@@ -1,12 +1,12 @@
 class Solution:
     def matrixMedian(self, grid: List[List[int]]) -> int:
-        def count(x):
-            return sum(bisect_right(row, x) for row in grid)
+        # def count(x):
+        #     return sum(bisect_right(row, x) for row in grid)
 
-        m, n = len(grid), len(grid[0])
-        target = (m * n + 1) >> 1
-        return bisect_left(range(10**6 + 1), target, key=count)
-        
+        # m, n = len(grid), len(grid[0])
+        # target = (m * n + 1) >> 1
+        # return bisect_left(range(10**6 + 1), target, key=count)
+
         m, n = len(grid), len(grid[0])        
         heap = []
         mid = m * n // 2 + 1
