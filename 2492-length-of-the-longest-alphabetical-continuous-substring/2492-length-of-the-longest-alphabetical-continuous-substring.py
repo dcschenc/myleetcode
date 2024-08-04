c50ff8f446e652df1415ec1617a6abf7092ac46f
dcschenc <dcschenc@gmail.com>
2024-08-04 15:49:47 +0800

    Time: 321 ms (30.61%), Space: 17.6 MB (16.56%) - LeetHub

--- 2492-length-of-the-longest-alphabetical-continuous-substring/2492-length-of-the-longest-alphabetical-continuous-substring.py
+++ 2492-length-of-the-longest-alphabetical-continuous-substring/2492-length-of-the-longest-alphabetical-continuous-substring.py
@@ -1,5 +1,6 @@
 class Solution:
     def longestContinuousSubstring(self, s: str) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/2400-2499/2414.Length%20of%20the%20Longest%20Alphabetical%20Continuous%20Substring
         l, r = 0, 1
         ans = 1
         while r < len(s):
