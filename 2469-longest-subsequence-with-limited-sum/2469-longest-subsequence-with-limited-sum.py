7d52d149ba37211cef8ff919ec0e778b2d55ecd8
dcschenc <dcschenc@gmail.com>
2024-08-04 14:23:50 +0800

    Time: 88 ms (81.48%), Space: 16.9 MB (31.48%) - LeetHub

--- 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.py
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         # https://github.com/doocs/leetcode/tree/main/solution/2300-2399/2389.Longest%20Subsequence%20With%20Limited%20Sum
-        # nums.sort()
-        # s = list(accumulate(nums))
-        # return [bisect_right(s, q) for q in queries]
+        nums.sort()
+        s = list(accumulate(nums))
+        return [bisect_right(s, q) for q in queries]
 
         nums.sort()
         ans, n = [], len(nums)
