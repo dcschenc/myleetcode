ebef1276fd8dbe4bf2420cf8039132158b4efdbd
dcschenc <dcschenc@gmail.com>
2024-08-04 15:04:55 +0800

    Time: 568 ms (88%), Space: 30.9 MB (69.33%) - LeetHub

--- 2535-count-strictly-increasing-subarrays/2535-count-strictly-increasing-subarrays.py
+++ 2535-count-strictly-increasing-subarrays/2535-count-strictly-increasing-subarrays.py
@@ -1,5 +1,16 @@
 class Solution:
     def countSubarrays(self, nums: List[int]) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/2300-2399/2393.Count%20Strictly%20Increasing%20Subarrays
+        ans = i = 0
+        while i < len(nums):
+            j = i + 1
+            while j < len(nums) and nums[j] > nums[j - 1]:
+                j += 1
+            cnt = j - i
+            ans += (1 + cnt) * cnt // 2
+            i = j
+        return ans
+
         n = len(nums)
         prectn = [1] * n
         for i in range(1,n):
