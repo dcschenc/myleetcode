41a81886e8e0b963dc90f5a37327efe587dc6a71
dcschenc <dcschenc@gmail.com>
2024-08-09 17:18:22 +0800

    Time: 79 ms (6.47%), Space: 16.5 MB (74.11%) - LeetHub

--- 2785-semi-ordered-permutation/2785-semi-ordered-permutation.py
+++ 2785-semi-ordered-permutation/2785-semi-ordered-permutation.py
@@ -1,6 +1,8 @@
 class Solution:
     def semiOrderedPermutation(self, nums: List[int]) -> int:
         # https://github.com/doocs/leetcode/tree/main/solution/2700-2799/2717.Semi-Ordered%20Permutation
+        # If 1 is to the left of n, the total moves will be the sum of both shifts.
+        # If 1 is to the right of n, subtract one from the total since moving 1 left also moves n closer to its final position.
         n = len(nums)
         mi, mx = min(nums), max(nums)        
         mi_idx, mx_idx = nums.index(mi), nums.index(mx)
