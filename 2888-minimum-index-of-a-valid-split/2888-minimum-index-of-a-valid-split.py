ec3038d70a3ddbd0232d11240085781b6354dbc8
dcschenc <dcschenc@gmail.com>
2024-08-11 16:07:05 +0800

    Time: 532 ms (51.8%), Space: 31.2 MB (77.7%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
@@ -1,6 +1,15 @@
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
         # https://github.com/doocs/leetcode/tree/main/solution/2700-2799/2780.Minimum%20Index%20of%20a%20Valid%20Split
+        x, cnt = Counter(nums).most_common(1)[0]
+        cur = 0
+        for i, v in enumerate(nums, 1):
+            if v == x:
+                cur += 1
+                if cur * 2 > i and (cnt - cur) * 2 > len(nums) - i:
+                    return i - 1
+        return -1
+        
         n = len(nums)
         t, c = Counter(nums).most_common()[0]
         cur = 0
