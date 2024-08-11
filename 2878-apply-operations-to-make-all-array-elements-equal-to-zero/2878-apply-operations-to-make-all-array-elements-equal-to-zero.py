fc6f47a69155b1f955109695259db8c4739069cc
dcschenc <dcschenc@gmail.com>
2024-08-11 15:55:14 +0800

    Time: 618 ms (61.99%), Space: 31.3 MB (15.79%) - LeetHub

---
+++ 2878-apply-operations-to-make-all-array-elements-equal-to-zero/2878-apply-operations-to-make-all-array-elements-equal-to-zero.py
@@ -0,0 +1,30 @@
+class Solution:
+    # https://github.com/doocs/leetcode/tree/main/solution/2700-2799/2772.Apply%20Operations%20to%20Make%20All%20Array%20Elements%20Equal%20to%20Zero
+    def checkArray(self, nums: List[int], k: int) -> bool:
+        n = len(nums)
+        deltas = [0] * n
+        decreased = 0
+        for i in range(n):
+            decreased -= deltas[i] # Remove the outdated window
+            actual = nums[i] - decreased 
+            if actual < 0 or (actual > 0 and i + k - 1 >= n):
+                return False
+            decreased += actual
+            if i + k < n:
+                deltas[i + k] += actual
+        return True       
+        
+        # i, n = 0, len(nums)
+        # while i <= n - k:
+        #     if nums[i] < 0:
+        #         return False
+        #     if nums[i] != 0:
+        #         tmp = nums[i]
+        #         for j in range(i, i+k):
+        #             nums[j] -= tmp            
+        #     i += 1
+        # for i in range(n-k, n):
+        #     if nums[i] != 0:
+        #         return False
+        # return True
+            
