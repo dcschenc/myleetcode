d6ee84334fbcab57273079652e8db5e440a7220f
dcschenc <dcschenc@gmail.com>
2024-08-11 18:23:20 +0800

    Time: 95 ms (61.36%), Space: 16.7 MB (91.32%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
@@ -1,28 +1,23 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        unique_elements = set(nums)
-        total_unique = len(unique_elements)
-        n = len(nums)
-        left = 0
+        total = len(set(nums))
+        left, result, n = 0, 0, len(nums)
         result = 0
-        current_window = {}
+        cur = Counter()
         
         for right in range(n):
             # Expand the window by including nums[right]
-            if nums[right] in current_window:
-                current_window[nums[right]] += 1
-            else:
-                current_window[nums[right]] = 1
+            cur[nums[right]] += 1
             
             # While the window contains all unique elements
-            while len(current_window) == total_unique:
+            while len(cur) == total:
                 # Count all subarrays from left to right
                 result += (n - right)
                 
                 # Move the left boundary of the window to shrink it
-                current_window[nums[left]] -= 1
-                if current_window[nums[left]] == 0:
-                    del current_window[nums[left]]
+                cur[nums[left]] -= 1
+                if cur[nums[left]] == 0:
+                    del cur[nums[left]]
                 left += 1
         
         return result
