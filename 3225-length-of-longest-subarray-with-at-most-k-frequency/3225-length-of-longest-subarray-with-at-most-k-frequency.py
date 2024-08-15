0dbad1498009a8e202c8bf86da4101ee3e05f47c
dcschenc <dcschenc@gmail.com>
2024-08-15 12:27:18 +0800

    Time: 1110 ms (88.62%), Space: 31.1 MB (59.6%) - LeetHub

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/3225-length-of-longest-subarray-with-at-most-k-frequency.py
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/3225-length-of-longest-subarray-with-at-most-k-frequency.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/2900-2999/2958.Length%20of%20Longest%20Subarray%20With%20at%20Most%20K%20Frequency
         cnt = defaultdict(int)
         ans = j = 0
         for i, x in enumerate(nums):
@@ -9,19 +10,19 @@ class Solution:
                 j += 1
             ans = max(ans, i - j + 1)
         return ans
-        
-        n, ans = len(nums), 0
-        start, hm = 0, defaultdict(deque)
-        for i in range(n):
-            cur = nums[i]
-            queue = hm[cur]
-            queue.append(i)
-            if len(queue) > k:
-                j = queue.popleft()
-                if j + 1 > start:
-                    start = j + 1            
-            ans = max(ans, i - start + 1)
+
+        # n, ans = len(nums), 0
+        # start, hm = 0, defaultdict(deque)
+        # for i in range(n):
+        #     cur = nums[i]
+        #     queue = hm[cur]
+        #     queue.append(i)
+        #     if len(queue) > k:
+        #         j = queue.popleft()
+        #         if j + 1 > start:
+        #             start = j + 1            
+        #     ans = max(ans, i - start + 1)
                 
-        return ans
+        # return ans
 
         
