f898c1dd35886a184f1839016faf8f6438682819
dcschenc <dcschenc@gmail.com>
2024-08-15 14:45:18 +0800

    Time: 48 ms (80.24%), Space: 16.5 MB (43.87%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.py
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.py
@@ -1,27 +1,6 @@
 from collections import defaultdict
 class Solution:
-    def maximumLength(self, s: str) -> int:      
-        def check(x: int) -> bool:
-            cnt = defaultdict(int)
-            i = 0
-            while i < n:
-                j = i + 1
-                while j < n and s[j] == s[i]:
-                    j += 1
-                cnt[s[i]] += max(0, j - i - x + 1)
-                i = j
-            return max(cnt.values()) >= 3
-
-        n = len(s)
-        l, r = 0, n
-        while l < r:
-            mid = (l + r + 1) >> 1
-            if check(mid):
-                l = mid
-            else:
-                r = mid - 1
-        return -1 if l == 0 else l
-         
+    def maximumLength(self, s: str) -> int:       
         hm = defaultdict(int)
         n, i = len(s), 0
         while i < n:
@@ -36,7 +15,7 @@ class Solution:
         ans = [len(k) for k, v in hm.items() if v >=3]
         ans.sort(reverse=True)
         return ans[0] if ans else -1
-
+        
         # hm = defaultdict(int)
         # n = len(s)
         # i = 1
