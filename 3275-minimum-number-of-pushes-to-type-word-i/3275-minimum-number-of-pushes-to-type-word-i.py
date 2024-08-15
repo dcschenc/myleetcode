fac1eda0422d30e22ff0a82dabedad84bde21b0a
dcschenc <dcschenc@gmail.com>
2024-08-15 18:53:51 +0800

    Time: 35 ms (62.97%), Space: 16.6 MB (30.27%) - LeetHub

--- 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.py
+++ 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.py
@@ -1,5 +1,14 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/3000-3099/3014.Minimum%20Number%20of%20Pushes%20to%20Type%20Word%20I
+        n = len(word)
+        ans, k = 0, 1
+        for _ in range(n // 8):
+            ans += k * 8
+            k += 1
+        ans += k * (n % 8)
+        return ans
+
         # counter = Counter(word)
         cur, ans = 1, 0
         # for k, v in sorted(counter.items(), key=lambda x:x[1], reverse=True):
