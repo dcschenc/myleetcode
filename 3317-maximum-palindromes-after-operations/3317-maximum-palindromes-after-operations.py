a9a3a8718e85bef872f97254dfd0ae8dd2207412
dcschenc <dcschenc@gmail.com>
2024-08-16 15:02:34 +0800

    Time: 110 ms (90.91%), Space: 17.3 MB (86.36%) - LeetHub

--- 3317-maximum-palindromes-after-operations/3317-maximum-palindromes-after-operations.py
+++ 3317-maximum-palindromes-after-operations/3317-maximum-palindromes-after-operations.py
@@ -1,26 +1,16 @@
 class Solution:
     def maxPalindromesAfterOperations(self, words: List[str]) -> int:
-        count = Counter(''.join(words))
-        odd,even = 0,0
+        n = len(words)
+        v = Counter(''.join(words)).most_common()     
+        pairs = sum([f//2 for _, f in v])        
+        sizes = [len(w) for w in words]
+        sizes.sort()        
         ans = 0
-        
-        for val in count.values():
-            even += val - val % 2
-            odd += val % 2
-
-        words.sort(key=len)
-
-        for word in words:
-            length = len(word)            
-            evenLenReq = length - (length%2)
-            oddLenReq = length %2
-
-            if (even>=evenLenReq and odd>=oddLenReq):
-                ans+=1
-                even-=evenLenReq
-                odd-=oddLenReq   
-            elif even>=length:
-                ans+=1
-                even-=length
-               
+        for i in range(n):
+            need = sizes[i]//2
+            if need <= pairs:
+                pairs -= need
+                ans += 1
+            else:
+                break        
         return ans
