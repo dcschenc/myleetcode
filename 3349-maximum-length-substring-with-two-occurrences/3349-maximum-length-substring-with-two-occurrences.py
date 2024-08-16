df89003ea0763b81ed7469e4bcb56d132182609f
dcschenc <dcschenc@gmail.com>
2024-08-16 19:49:04 +0800

    Time: 51 ms (35.22%), Space: 16.6 MB (32.24%) - LeetHub

--- 3349-maximum-length-substring-with-two-occurrences/3349-maximum-length-substring-with-two-occurrences.py
+++ 3349-maximum-length-substring-with-two-occurrences/3349-maximum-length-substring-with-two-occurrences.py
@@ -1,5 +1,16 @@
 class Solution:
-    def maximumLengthSubstring(self, s: str) -> int:        
+    def maximumLengthSubstring(self, s: str) -> int:     
+        # https://github.com/doocs/leetcode/tree/main/solution/3000-3099/3090.Maximum%20Length%20Substring%20With%20Two%20Occurrences
+        cnt = Counter()
+        ans = i = 0
+        for j, c in enumerate(s):
+            cnt[c] += 1
+            while cnt[c] > 2:
+                cnt[s[i]] -= 1
+                i += 1
+            ans = max(ans, j - i + 1)
+        return ans
+
         n = len(s)
         mx = 0
         for i in range(n):
