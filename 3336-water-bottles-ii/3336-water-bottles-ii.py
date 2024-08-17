101d6391f54b4c543471dc4000a5a86ae7adde9e
dcschenc <dcschenc@gmail.com>
2024-08-17 09:31:20 +0800

    Time: 36 ms (60.85%), Space: 16.5 MB (30.69%) - LeetHub

--- 3336-water-bottles-ii/3336-water-bottles-ii.py
+++ 3336-water-bottles-ii/3336-water-bottles-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/3100-3199/3100.Water%20Bottles%20II
         ans, empty_bottles = 0, 0
         while numBottles > 0:
             ans += numBottles
