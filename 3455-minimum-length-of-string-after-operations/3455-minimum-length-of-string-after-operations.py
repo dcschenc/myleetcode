6f9121aff4f13d1c08bfbe9025bb1c7bfe14ed47
dcschenc <dcschenc@gmail.com>
2024-08-19 19:37:42 +0800

    Time: 192 ms (74.18%), Space: 18.9 MB (84.84%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
@@ -1,5 +1,6 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/3200-3299/3223.Minimum%20Length%20of%20String%20After%20Operations
         counter = Counter(s)
         total = 0
         for k, v in counter.items():
