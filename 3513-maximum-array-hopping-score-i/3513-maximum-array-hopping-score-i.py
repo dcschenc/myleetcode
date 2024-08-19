1cef839400a96143413273902668218120448ca5
dcschenc <dcschenc@gmail.com>
2024-08-19 14:46:37 +0800

    Time: 5520 ms (46.93%), Space: 19.6 MB (15.2%) - LeetHub

--- 3513-maximum-array-hopping-score-i/3513-maximum-array-hopping-score-i.py
+++ 3513-maximum-array-hopping-score-i/3513-maximum-array-hopping-score-i.py
@@ -2,6 +2,14 @@ class Solution:
     def maxScore(self, nums: List[int]) -> int:
         # https://github.com/doocs/leetcode/tree/main/solution/3200-3299/3205.Maximum%20Array%20Hopping%20Score%20I
         @cache
+        def dfs(i: int) -> int:
+            return max(
+                [(j - i) * nums[j] + dfs(j) for j in range(i + 1, len(nums))] or [0]
+            )
+
+        return dfs(0)
+        
+        @cache
         def dfs(i):
             if i >= n - 1:
                 return 0
