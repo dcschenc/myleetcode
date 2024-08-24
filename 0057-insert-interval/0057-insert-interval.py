b2cf6a2059c16efaa453e4db3187d57b84dfe503
dcschenc <dcschenc@gmail.com>
2024-08-24 11:24:02 +0800

    Time: 74 ms (63.53%), Space: 19.9 MB (71.21%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,39 +1,38 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        res = []
-        for i, interval in enumerate(intervals):
-            if newInterval[1] < interval[0]:
-                res.append(newInterval)
-                return res + intervals[i:]
-            elif newInterval[0] > interval[1]:
-                res.append(interval)
-            else: ### for overlap
-                left = min(interval[0], newInterval[0])
-                right = max(interval[1], newInterval[1])
-                newInterval = [left, right]
-        res.append(newInterval)
-        return res
+        # https://github.com/doocs/leetcode/tree/main/solution/0000-0099/0057.Insert%20Interval
+        def merge(intervals: List[List[int]]) -> List[List[int]]:
+            intervals.sort()
+            ans = [intervals[0]]
+            for s, e in intervals[1:]:
+                if ans[-1][1] < s:
+                    ans.append([s, e])
+                else:
+                    ans[-1][1] = max(ans[-1][1], e)
+            return ans
 
+        intervals.append(newInterval)
+        return merge(intervals)
         
-        n = len(intervals)
-        i = 0
-        res = []
+        # n = len(intervals)
+        # i = 0
+        # res = []
 
-        # Case 1: No overlapping before merging intervals
-        while i < n and intervals[i][1] < newInterval[0]:
-            res.append(intervals[i])
-            i += 1
+        # # Case 1: No overlapping before merging intervals
+        # while i < n and intervals[i][1] < newInterval[0]:
+        #     res.append(intervals[i])
+        #     i += 1
 
-        # Case 2: Overlapping and merging intervals
-        while i < n and newInterval[1] >= intervals[i][0]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
-            i += 1
-        res.append(newInterval)
+        # # Case 2: Overlapping and merging intervals
+        # while i < n and newInterval[1] >= intervals[i][0]:
+        #     newInterval[0] = min(newInterval[0], intervals[i][0])
+        #     newInterval[1] = max(newInterval[1], intervals[i][1])
+        #     i += 1
+        # res.append(newInterval)
 
-        # Case 3: No overlapping after merging newInterval
-        while i < n:
-            res.append(intervals[i])
-            i += 1
+        # # Case 3: No overlapping after merging newInterval
+        # while i < n:
+        #     res.append(intervals[i])
+        #     i += 1
 
-        return res
+        # return res
