4eece1895b2672e3e51f0131f86379bd3f3f0d82
dcschenc <dcschenc@gmail.com>
2024-08-24 11:29:18 +0800

    Time: 48 ms (6.97%), Space: 16.7 MB (20.46%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -5,22 +5,43 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if head is None:
-            return None
+        # https://github.com/doocs/leetcode/tree/main/solution/0000-0099/0061.Rotate%20List
+        if head is None or head.next is None:
+            return head
+        cur, n = head, 0
+        while cur:
+            n += 1
+            cur = cur.next
+        k %= n
+        if k == 0:
+            return head
+        fast = slow = head
+        for _ in range(k):
+            fast = fast.next
+        while fast.next:
+            fast, slow = fast.next, slow.next
+
+        ans = slow.next
+        slow.next = None
+        fast.next = head
+        return ans
+
+        # if head is None:
+        #     return None
             
-        node = head
-        count = 1
-        while node.next:
-            count += 1
-            node = node.next
-        # node.next = head
-        last_node = node
-        node = head
-        k = count - k % count
-        while k > 1:
-            node = node.next
-            k -= 1
-        last_node.next = head
-        head = node.next
-        node.next = None
-        return head
+        # node = head
+        # count = 1
+        # while node.next:
+        #     count += 1
+        #     node = node.next
+        # # node.next = head
+        # last_node = node
+        # node = head
+        # k = count - k % count
+        # while k > 1:
+        #     node = node.next
+        #     k -= 1
+        # last_node.next = head
+        # head = node.next
+        # node.next = None
+        # return head
