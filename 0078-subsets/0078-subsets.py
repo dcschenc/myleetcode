283a6864b614caee9ab5c0075d543cd91f1e3154
dcschenc <dcschenc@gmail.com>
2024-08-25 21:57:49 +0800

    Time: 35 ms (72.47%), Space: 16.7 MB (24.86%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,23 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ########## O(2^N) ########
-        # def backtrack(nums, curr):
-        #     res.append(curr)
-        #     for i, v in enumerate(nums):
-        #         backtrack(nums[i+1:], curr+[v])
-        
-        # res = []
-        # backtrack(nums, [])
-        # return res
-        
         def backtrack(start, path):
             result.append(path[:])
             if start == n: 
                 return            
             for i in range(start, len(nums)):
-                # current_subset.append(nums[i])
                 backtrack(i + 1, path + [nums[i]])
-                # current_subset.pop()
         
         n, result = len(nums), []
         backtrack(0, [])
