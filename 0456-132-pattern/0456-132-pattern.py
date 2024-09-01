fc007e9085b6a76f516b96a9b359fde6487ae901
dcschenc <dcschenc@gmail.com>
2024-09-01 14:34:57 +0800

    Time: 522 ms (63.75%), Space: 37 MB (40.81%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -1,21 +1,30 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
         # https://github.com/doocs/leetcode/tree/main/solution/0400-0499/0456.132%20Pattern
+        if len(nums) < 3:
+            return False
+        
+        stack = []
+        second = float('-inf')
+        
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] < second:
+                return True
+            while stack and stack[-1] < nums[i]:
+                second = stack.pop()
+            stack.append(nums[i])
+        
+        return False
+        
         n = len(nums)
         if n < 3: return False
-
-        # Initialize the stack to store elements in decreasing order.
         stack = []
         third = float('-inf')  
-        # Iterate the array from right to left.
         for i in range(n - 1, -1, -1):
             if nums[i] < third:  return True
-            # update the third element with the top of the stack and pop the stack.
             while stack and nums[i] > stack[-1]:
                 third = stack.pop()
-
-            # Push the current element onto the stack.
             stack.append(nums[i])
 
-        return False  # No valid subsequence was found.
+        return False  
 
