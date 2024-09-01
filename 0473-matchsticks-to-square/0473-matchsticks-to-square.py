5aa345fd133a8d6fb50c4ad8bac259c17af2e427
dcschenc <dcschenc@gmail.com>
2024-09-01 18:31:59 +0800

    Time: 3902 ms (34.72%), Space: 16.6 MB (79.65%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.py
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.py
@@ -2,15 +2,15 @@ class Solution:
     def makesquare(self, matchsticks: List[int]) -> bool:
         def dfs(i):
             if i == len(matchsticks):
-                if s[0] == s[1] and s[1] == s[2] and s[2] == s[3]:                    
-                    return True                
-                return False
+                return s[0] == s[1] == s[2] == s[3]                  
+                   
                 
             for j in range(4):   
                 if s[j] + matchsticks[i] > target:
                     continue     
                 s[j] += matchsticks[i]   
-                if dfs(i + 1): return True
+                if dfs(i + 1): 
+                    return True
                 s[j] -= matchsticks[i]               
             return False
 
