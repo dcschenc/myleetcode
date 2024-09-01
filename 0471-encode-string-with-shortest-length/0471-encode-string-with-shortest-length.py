41bebca6ffc858a78e761408bc1c279599829fc6
dcschenc <dcschenc@gmail.com>
2024-09-01 16:34:16 +0800

    Time: 126 ms (88.54%), Space: 17 MB (92.71%) - LeetHub

--- 0471-encode-string-with-shortest-length/0471-encode-string-with-shortest-length.py
+++ 0471-encode-string-with-shortest-length/0471-encode-string-with-shortest-length.py
@@ -1,6 +1,6 @@
 class Solution:
     def encode(self, s: str) -> str:
-        # Helper method to compute the encoded string for a substring s[i:j+1]
+        # https://github.com/doocs/leetcode/tree/main/solution/0400-0499/0471.Encode%20String%20with%20Shortest%20Length
         def compute_encoded_substring(start: int, end: int) -> str:
             substring = s[start:end + 1]
             # If the substring is too short, encoding doesn't make sense, return as is
@@ -15,21 +15,16 @@ class Solution:
                 count_of_repetition = len(substring) // duplicate_index
                 # Encoded format: count[encoded substring for the repeated pattern]
                 return f"{count_of_repetition}[{dp[start][start + duplicate_index - 1]}]"
-            # No repeated pattern was found, return the original substring
             return substring
 
-        # Main logic begins here
         n = len(s)
-        # Initialize dynamic programming table with None
         dp = [[None] * n for _ in range(n)]
       
         # Build the table bottom-up
         for i in range(n - 1, -1, -1):
             for j in range(i, n):
                 # Compute encoded substring for current i, j
-                dp[i][j] = compute_encoded_substring(i, j)
-              
-                # If current substring can potentially be shortened
+                dp[i][j] = compute_encoded_substring(i, j)              
                 if j - i + 1 > 4:
                     # Try to break the substring into two parts and see if this gives
                     # a shorter encoding by checking all possible split positions
@@ -40,5 +35,4 @@ class Solution:
                         if len(dp[i][j]) > len(trial_encoded):
                             dp[i][j] = trial_encoded
       
-        # Result is in the top-right cell of the dynamic programming table
         return dp[0][-1]        
