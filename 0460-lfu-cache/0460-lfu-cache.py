2c3a6d497d606f7817540bf515530988ff4a4a71
dcschenc <dcschenc@gmail.com>
2024-09-01 15:35:12 +0800

    Time: 552 ms (83.9%), Space: 81.7 MB (18.05%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.py
+++ 0460-lfu-cache/0460-lfu-cache.py
@@ -1,35 +1,35 @@
-class Node:
-    def __init__(self, key: int, value: int) -> None:
-        self.key = key
-        self.value = value
-        self.freq = 1
-        self.prev = None
-        self.next = None
+# class Node:
+#     def __init__(self, key: int, value: int) -> None:
+#         self.key = key
+#         self.value = value
+#         self.freq = 1
+#         self.prev = None
+#         self.next = None
 
-class DoublyLinkedList:
-    def __init__(self) -> None:
-        self.head = Node(-1, -1)
-        self.tail = Node(-1, -1)
-        self.head.next = self.tail
-        self.tail.prev = self.head
+# class DoublyLinkedList:
+#     def __init__(self) -> None:
+#         self.head = Node(-1, -1)
+#         self.tail = Node(-1, -1)
+#         self.head.next = self.tail
+#         self.tail.prev = self.head
 
-    def add_first(self, node: Node) -> None:
-        node.prev = self.head
-        node.next = self.head.next
-        self.head.next.prev = node
-        self.head.next = node
+#     def add_first(self, node: Node) -> None:
+#         node.prev = self.head
+#         node.next = self.head.next
+#         self.head.next.prev = node
+#         self.head.next = node
 
-    def remove(self, node: Node) -> Node:
-        node.next.prev = node.prev
-        node.prev.next = node.next
-        node.next, node.prev = None, None
-        return node
+#     def remove(self, node: Node) -> Node:
+#         node.next.prev = node.prev
+#         node.prev.next = node.next
+#         node.next, node.prev = None, None
+#         return node
 
-    def remove_last(self) -> Node:
-        return self.remove(self.tail.prev)
+#     def remove_last(self) -> Node:
+#         return self.remove(self.tail.prev)
 
-    def is_empty(self) -> bool:
-        return self.head.next == self.tail
+#     def is_empty(self) -> bool:
+#         return self.head.next == self.tail
 
 
 class LFUCache:
@@ -37,50 +37,55 @@ class LFUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.min_freq = 0
-        self.map = defaultdict(Node)
-        self.freq_map = defaultdict(DoublyLinkedList)
+        self.key_to_val = {}
+        self.key_to_freq = {}
+        self.freq_to_keys = defaultdict(OrderedDict)
 
     def get(self, key: int) -> int:
-        if self.capacity == 0 or key not in self.map:
+        if key not in self.key_to_val:
             return -1
-        node = self.map[key]
-        self.incr_freq(node)
-        return node.value
+
+        # Get the value and the current frequency
+        value = self.key_to_val[key]
+        freq = self.key_to_freq[key]
+
+        # Remove the key from the current frequency list
+        del self.freq_to_keys[freq][key]
+
+        # If the current min frequency list is empty, update the min frequency
+        if not self.freq_to_keys[freq]:
+            del self.freq_to_keys[freq]
+            if freq == self.min_freq:
+                self.min_freq += 1
+
+        # Update the key's frequency
+        self.key_to_freq[key] = freq + 1
+        self.freq_to_keys[freq + 1][key] = value
+
+        return value
 
     def put(self, key: int, value: int) -> None:
         if self.capacity == 0:
             return
-        if key in self.map:
-            node = self.map[key]
-            node.value = value
-            self.incr_freq(node)
+
+        if key in self.key_to_val:
+            # Update the value
+            self.key_to_val[key] = value
+            # Use the get function to update the frequency of the key
+            self.get(key)
             return
-        if len(self.map) == self.capacity:
-            ls = self.freq_map[self.min_freq]
-            node = ls.remove_last()
-            self.map.pop(node.key)
-        node = Node(key, value)
-        self.add_node(node)
-        self.map[key] = node
+
+        if len(self.key_to_val) >= self.capacity:
+            # Evict the least frequently used key
+            evict_key, _ = self.freq_to_keys[self.min_freq].popitem(last=False)
+            del self.key_to_val[evict_key]
+            del self.key_to_freq[evict_key]
+
+        # Add the new key-value pair
+        self.key_to_val[key] = value
+        self.key_to_freq[key] = 1
+        self.freq_to_keys[1][key] = value
         self.min_freq = 1
-
-    def incr_freq(self, node: Node) -> None:
-        freq = node.freq
-        ls = self.freq_map[freq]
-        ls.remove(node)
-        if ls.is_empty():
-            self.freq_map.pop(freq)
-            if freq == self.min_freq:
-                self.min_freq += 1
-        node.freq += 1
-        self.add_node(node)
-
-    def add_node(self, node: Node) -> None:
-        freq = node.freq
-        ls = self.freq_map[freq]
-        ls.add_first(node)
-        self.freq_map[freq] = ls
-
 
 # Your LFUCache object will be instantiated and called as such:
 # obj = LFUCache(capacity)
