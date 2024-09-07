232f5590d7b0e66bd977ea23b51e23088a66e1a6
dcschenc <dcschenc@gmail.com>
2024-09-07 19:53:44 +0800

    Time: 40 ms (27.22%), Space: 16.4 MB (69.59%) - LeetHub

--- 0824-number-of-lines-to-write-string/0824-number-of-lines-to-write-string.py
+++ 0824-number-of-lines-to-write-string/0824-number-of-lines-to-write-string.py
@@ -1,18 +1,15 @@
 class Solution:
     def numberOfLines(self, widths: List[int], s: str) -> List[int]:
-        # widths = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
         res = [0, 0]
-        curr = 0
+        cur = 0
         cnt = 1
         for c in s:
             idx = ord(c) - ord('a')
-            if widths[idx] + curr > 100:
-                curr = widths[idx]
+            if widths[idx] + cur > 100:
+                cur = widths[idx]
                 cnt += 1
             else:
-                curr += widths[idx]
-        res[0] = cnt
-        res[1] = curr
-        return res
+                cur += widths[idx]
+        return [cnt, cur]
 
 
