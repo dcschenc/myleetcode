3f213836ea78ee8182da2ed8b34367b36b5f2cc1
dcschenc <dcschenc@gmail.com>
2024-09-07 09:01:16 +0800

    Time: 46 ms (56.92%), Space: 16.4 MB (98.46%) - LeetHub

--- 0793-swap-adjacent-in-lr-string/0793-swap-adjacent-in-lr-string.py
+++ 0793-swap-adjacent-in-lr-string/0793-swap-adjacent-in-lr-string.py
@@ -1,37 +1,21 @@
 class Solution:
     def canTransform(self, start: str, end: str) -> bool:
-        # n = len(start)
-        # i = j = 0
-        # while 1:
-        #     while i < n and start[i] == 'X':
-        #         i += 1
-        #     while j < n and end[j] == 'X':
-        #         j += 1
-        #     if i >= n and j >= n:
-        #         return True
-        #     if i >= n or j >= n or start[i] != end[j]:
-        #         return False
-        #     if start[i] == 'L' and i < j:
-        #         return False
-        #     if start[i] == 'R' and i > j:
-        #         return False
-        #     i, j = i + 1, j + 1
+        # https://github.com/doocs/leetcode/tree/main/solution/0700-0799/0777.Swap%20Adjacent%20in%20LR%20String
+        n = len(start)
+        i = j = 0
+        while 1:
+            while i < n and start[i] == 'X':
+                i += 1
+            while j < n and end[j] == 'X':
+                j += 1
+            if i >= n and j >= n:
+                return True
+            if i >= n or j >= n or start[i] != end[j]:
+                return False
+            if start[i] == 'L' and i < j:
+                return False
+            if start[i] == 'R' and i > j:
+                return False
+            i, j = i + 1, j + 1
 
-        a, b = [], []
-        for i, c in enumerate(start):
-            if c != 'X':
-                a.append((c, i))
-        for i, c in enumerate(end):
-            if c != 'X':
-                b.append((c, i))
-        if len(a) != len(b):
-            return False
-        
-        for (x, i), (y, j) in zip(a, b):
-            if x != y:
-                return False
-            if x == 'L' and i < j:
-                return False
-            if x == 'R' and i > j:
-                return False
-        return True
+    
