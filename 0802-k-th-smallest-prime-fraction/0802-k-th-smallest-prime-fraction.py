c13b178ec44cfe0291566327ced65fd77e192ee2
dcschenc <dcschenc@gmail.com>
2024-09-07 10:49:14 +0800

    Time: 550 ms (76.94%), Space: 16.9 MB (61.84%) - LeetHub

--- 0802-k-th-smallest-prime-fraction/0802-k-th-smallest-prime-fraction.py
+++ 0802-k-th-smallest-prime-fraction/0802-k-th-smallest-prime-fraction.py
@@ -6,25 +6,16 @@ class Solution:
         
         # Initialize the heap with the smallest fractions (arr[0] / arr[j], j > 0)
         for j in range(1, n):
-            heapq.heappush(heap, (arr[0] / arr[j], 0, j))
+            heappush(heap, (arr[0] / arr[j], 0, j))
         
         # Extract the smallest fractions k times
         for _ in range(k - 1):
-            frac, i, j = heapq.heappop(heap)
+            frac, i, j = heappop(heap)
             # Push the next fraction (arr[i+1] / arr[j]) if possible
             if i + 1 < j:
-                heapq.heappush(heap, (arr[i+1] / arr[j], i + 1, j))
-        
+                heappush(heap, (arr[i+1] / arr[j], i + 1, j))
+
         # The k-th fraction
-        _, i, j = heapq.heappop(heap)
+        _, i, j = heappop(heap)
         return [arr[i], arr[j]]
 
-
-        # n = len(arr)
-        # heap = []
-        # for i in range(n):
-        #     for j in range(i+1, n):
-        #         heapq.heappush(heap, (-arr[i]/arr[j], [arr[i], arr[j]]))
-        #         if len(heap) > k:
-        #             heapq.heappop(heap)
-        # return heap[0][1]
