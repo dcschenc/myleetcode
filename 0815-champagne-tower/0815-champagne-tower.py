0c1ff09cabd817275824b65279b88399dd611f31
dcschenc <dcschenc@gmail.com>
2024-09-07 15:07:21 +0800

    Time: 74 ms (52.97%), Space: 16.7 MB (54.95%) - LeetHub

--- 0815-champagne-tower/0815-champagne-tower.py
+++ 0815-champagne-tower/0815-champagne-tower.py
@@ -1,24 +1,24 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        f = [[0] * 101 for _ in range(101)]
-        f[0][0] = poured
+        dp = [[0] * 101 for _ in range(101)]
+        dp[0][0] = poured
         for i in range(query_row + 1):
             for j in range(i + 1):
-                if f[i][j] > 1:
-                    half = (f[i][j] - 1) / 2
-                    f[i][j] = 1
-                    f[i + 1][j] += half
-                    f[i + 1][j + 1] += half
-        return f[query_row][query_glass]
+                if dp[i][j] > 1:
+                    half = (dp[i][j] - 1) / 2
+                    dp[i][j] = 1
+                    dp[i + 1][j] += half
+                    dp[i + 1][j + 1] += half
+        return dp[query_row][query_glass]
 
-        n = 101
-        dp = [[0 for j in range(n)] for i in range(n)]
-        dp[1][1] = poured
-        for i in range(2, n):
-            for j in range(1, i + 1):
-                if dp[i-1][j] > 1:
-                    dp[i][j] += (dp[i-1][j] - 1)/2
-                if dp[i-1][j-1] > 1:
-                    dp[i][j] += (dp[i-1][j-1] - 1)/2
-        # print(dp)
-        return dp[query_row + 1][query_glass + 1] if dp[query_row + 1][query_glass + 1] < 1 else 1
+        # n = 101
+        # dp = [[0 for j in range(n)] for i in range(n)]
+        # dp[1][1] = poured
+        # for i in range(2, n):
+        #     for j in range(1, i + 1):
+        #         if dp[i-1][j] > 1:
+        #             dp[i][j] += (dp[i-1][j] - 1)/2
+        #         if dp[i-1][j-1] > 1:
+        #             dp[i][j] += (dp[i-1][j-1] - 1)/2
+        # # print(dp)
+        # return dp[query_row + 1][query_glass + 1] if dp[query_row + 1][query_glass + 1] < 1 else 1
