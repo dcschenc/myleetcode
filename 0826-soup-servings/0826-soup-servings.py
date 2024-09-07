7a49ffc2204c67ab15cf855ad9d643c51153752f
dcschenc <dcschenc@gmail.com>
2024-09-07 20:22:11 +0800

    Time: 40 ms (89.32%), Space: 18.1 MB (73.14%) - LeetHub

--- 0826-soup-servings/0826-soup-servings.py
+++ 0826-soup-servings/0826-soup-servings.py
@@ -8,11 +8,7 @@ class Solution:
                 return 1
             if j <= 0:
                 return 0
-            return 0.25 * (
-                dfs(i - 4, j)
-                + dfs(i - 3, j - 1)
-                + dfs(i - 2, j - 2)
-                + dfs(i - 1, j - 3)
-            )
+                
+            return 0.25 * (dfs(i - 4, j)+ dfs(i - 3, j - 1) + dfs(i - 2, j - 2) + dfs(i - 1, j - 3))
 
         return 1 if n > 4800 else dfs((n + 24) // 25, (n + 24) // 25)
