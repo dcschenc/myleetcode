d0f6abec686b0b32ad199392f649511f147a4894
dcschenc <dcschenc@gmail.com>
2024-09-07 09:08:56 +0800

    Time: 42 ms (79.69%), Space: 16.6 MB (79.38%) - LeetHub

--- 0793-swap-adjacent-in-lr-string/0793-swap-adjacent-in-lr-string.py
+++ 0793-swap-adjacent-in-lr-string/0793-swap-adjacent-in-lr-string.py
@@ -1,21 +1,38 @@
 class Solution:
     def canTransform(self, start: str, end: str) -> bool:
         # https://github.com/doocs/leetcode/tree/main/solution/0700-0799/0777.Swap%20Adjacent%20in%20LR%20String
+        # First, check if the non-'X' characters match
+        if start.replace('X', '') != end.replace('X', ''):
+            return False
+        
+        # Now, check the valid moves for 'L' and 'R'
+        i, j = 0, 0
         n = len(start)
-        i = j = 0
-        while 1:
+        
+        while i < n and j < n:
+            # Skip all 'X' in both strings
             while i < n and start[i] == 'X':
                 i += 1
             while j < n and end[j] == 'X':
                 j += 1
-            if i >= n and j >= n:
-                return True
-            if i >= n or j >= n or start[i] != end[j]:
+            
+            # If one string is exhausted while the other isn't, it's not possible
+            if (i < n) != (j < n):
                 return False
-            if start[i] == 'L' and i < j:
-                return False
-            if start[i] == 'R' and i > j:
-                return False
-            i, j = i + 1, j + 1
+            
+            # If both pointers are valid, check the positions
+            if i < n and j < n:
+                if start[i] != end[j]:
+                    return False
+                # For 'L', i must be >= j since 'L' can only move left
+                if start[i] == 'L' and i < j:
+                    return False
+                # For 'R', i must be <= j since 'R' can only move right
+                if start[i] == 'R' and i > j:
+                    return False
+                i += 1
+                j += 1
+        
+        return True
 
     
