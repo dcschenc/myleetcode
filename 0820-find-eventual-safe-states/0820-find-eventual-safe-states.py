71981fda0e9ff6b44912e194384a47469885bbe7
dcschenc <dcschenc@gmail.com>
2024-09-07 19:50:07 +0800

    Time: 549 ms (29.81%), Space: 24.6 MB (19.3%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -2,16 +2,17 @@ from collections import deque, defaultdict
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:  
         # https://github.com/doocs/leetcode/tree/main/solution/0800-0899/0802.Find%20Eventual%20Safe%20States
-        rg = defaultdict(list)
+        nbs = defaultdict(list)
         indeg = [0] * len(graph)
         for i, vs in enumerate(graph):
             for j in vs:
-                rg[j].append(i)
+                nbs[j].append(i)
             indeg[i] = len(vs)
         q = deque([i for i, v in enumerate(indeg) if v == 0])
+
         while q:
             i = q.popleft()
-            for j in rg[i]:
+            for j in nbs[i]:
                 indeg[j] -= 1
                 if indeg[j] == 0:
                     q.append(j)
@@ -40,30 +41,3 @@ class Solution:
             dfs(i)
 
         return sorted([i for i in safe if safe[i]])
-
-        # def dfs(node):
-        #     if node in safe:
-        #         return True
-        #     if node in path:
-        #         return False
-        #     if len(graph[node]) == 0:
-        #         safe.add(node)
-        #         return True
-        #     path.add(node)
-        #     for nb in graph[node]:
-        #         res = dfs(nb)
-        #         if not res:
-        #             return False
-        #     # path.remove(node)  # Backtrack
-        #     safe.add(node)
-        #     return True
-
-        # safe = set()
-        # for i in range(len(graph)):
-        #     path = set()
-        #     if i not in safe:
-        #         dfs_result = dfs(i)
-        #         if dfs_result:
-        #             safe.add(i)
-
-        # return sorted(safe)
