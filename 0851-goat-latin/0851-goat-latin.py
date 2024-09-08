2829ea47cce91d14b5e86c691bd7ea27e2268c2e
dcschenc <dcschenc@gmail.com>
2024-09-08 10:29:42 +0800

    Time: 38 ms (34.18%), Space: 16.5 MB (70.77%) - LeetHub

--- 0851-goat-latin/0851-goat-latin.py
+++ 0851-goat-latin/0851-goat-latin.py
@@ -1,6 +1,6 @@
 class Solution:
     def toGoatLatin(self, sentence: str) -> str:
-        words = sentence.split(' ')
+        words = sentence.split()
         for i in range(len(words)):
             if words[i][0].lower() not in 'aeiou':                
                 words[i] = words[i][1:] + words[i][0]
