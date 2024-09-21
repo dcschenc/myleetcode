438e6c31ca070b1f5ff82052ecdd37f967cee3d0
dcschenc <dcschenc@gmail.com>
2024-09-21 14:29:24 +0800

    Time: 2492 ms (54.24%), Space: 16.6 MB (69.49%) - LeetHub

--- 1077-confusing-number-ii/1077-confusing-number-ii.py
+++ 1077-confusing-number-ii/1077-confusing-number-ii.py
@@ -1,5 +1,28 @@
 class Solution:
     def confusingNumberII(self, n: int) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/1000-1099/1088.Confusing%20Number%20II
+        def check(x: int) -> bool:
+            y, t = 0, x
+            while t:
+                t, v = divmod(t, 10)
+                y = y * 10 + d[v]
+            return x != y
+
+        def dfs(pos: int, limit: bool, x: int) -> int:
+            if pos >= len(s):
+                return int(check(x))
+            up = int(s[pos]) if limit else 9
+            ans = 0
+            for i in range(up + 1):
+                if d[i] != -1:
+                    ans += dfs(pos + 1, limit and i == up, x * 10 + i)
+            return ans
+
+        d = [0, 1, -1, -1, -1, -1, 9, -1, 8, 6]
+        s = str(n)
+        return dfs(0, True, 0)
+
+
         def is_confusing(n_str: int) -> bool:
             # special case - single digit
             if len(n_str)==1:
