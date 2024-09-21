7eaafe3686784ffad9b96691db1dbafbd4499776
dcschenc <dcschenc@gmail.com>
2024-09-21 15:10:16 +0800

    Time: 516 ms (26.61%), Space: 18.3 MB (5.69%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,6 +1,7 @@
 from collections import deque
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/1000-1099/1091.Shortest%20Path%20in%20Binary%20Matrix
         m, n = len(grid), len(grid[0])
         queue = deque()
         if grid[0][0] != 0:
@@ -22,21 +23,21 @@ class Solution:
                         queue.append((newX, newY, length + 1))
         return -1
 
-        if grid[0][0]:
-            return -1
-        n = len(grid)
-        grid[0][0] = 1
-        q = deque([(0, 0)])
-        ans = 1
-        while q:
-            for _ in range(len(q)):
-                i, j = q.popleft()
-                if i == j == n - 1:
-                    return ans
-                for x in range(i - 1, i + 2):
-                    for y in range(j - 1, j + 2):
-                        if 0 <= x < n and 0 <= y < n and grid[x][y] == 0:
-                            grid[x][y] = 1
-                            q.append((x, y))
-            ans += 1
-        return -1
+        # if grid[0][0]:
+        #     return -1
+        # n = len(grid)
+        # grid[0][0] = 1
+        # q = deque([(0, 0)])
+        # ans = 1
+        # while q:
+        #     for _ in range(len(q)):
+        #         i, j = q.popleft()
+        #         if i == j == n - 1:
+        #             return ans
+        #         for x in range(i - 1, i + 2):
+        #             for y in range(j - 1, j + 2):
+        #                 if 0 <= x < n and 0 <= y < n and grid[x][y] == 0:
+        #                     grid[x][y] = 1
+        #                     q.append((x, y))
+        #     ans += 1
+        # return -1
