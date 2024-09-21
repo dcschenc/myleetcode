1277dbb7a1ce7defb9114c6fe508f25fdebbe4a7
dcschenc <dcschenc@gmail.com>
2024-09-21 11:02:33 +0800

    Time: 37 ms (42.37%), Space: 16.6 MB (30.79%) - LeetHub

--- 1156-occurrences-after-bigram/1156-occurrences-after-bigram.py
+++ 1156-occurrences-after-bigram/1156-occurrences-after-bigram.py
@@ -1,7 +1,7 @@
 class Solution:
     def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
         ans = []
-        arr = text.split(' ')
+        arr = text.split()
         for i in range(1, len(arr)):
             if arr[i-1] == first and arr[i] == second and i+1 < len(arr):
                 ans.append(arr[i+1])
