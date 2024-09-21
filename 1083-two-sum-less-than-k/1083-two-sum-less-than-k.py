66fdf9ed81d39e8024f43674bb943ad49f1e4416
dcschenc <dcschenc@gmail.com>
2024-09-21 15:19:40 +0800

    Time: 44 ms (59.78%), Space: 16.6 MB (39.8%) - LeetHub

--- 1083-two-sum-less-than-k/1083-two-sum-less-than-k.py
+++ 1083-two-sum-less-than-k/1083-two-sum-less-than-k.py
@@ -1,5 +1,15 @@
 class Solution:
     def twoSumLessThanK(self, nums: List[int], k: int) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/1000-1099/1099.Two%20Sum%20Less%20Than%20K
+        nums.sort()
+        ans = -1
+        for i, x in enumerate(nums):
+            j = bisect_left(nums, k - x, lo=i + 1) - 1
+            if i < j:
+                ans = max(ans, x + nums[j])
+        return ans
+
+
         nums.sort()
         left, right = 0, len(nums) - 1
         max_sum = -1
@@ -11,10 +21,3 @@ class Solution:
             else:
                 right -= 1
         return max_sum
-
-        # max_sum = -1
-        # for i in range(len(nums)):
-        #     for j in range(i+1, len(nums)):
-        #         if nums[i] + nums[j] < k:
-        #             max_sum = max(max_sum, nums[i] + nums[j])
-        # return max_sum
