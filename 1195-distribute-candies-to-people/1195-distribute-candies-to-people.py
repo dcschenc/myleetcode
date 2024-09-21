537823f54730f5df2cb553347ec2a5c09f3b128a
dcschenc <dcschenc@gmail.com>
2024-09-21 15:46:16 +0800

    Time: 29 ms (98.09%), Space: 16.6 MB (39.93%) - LeetHub

--- 1195-distribute-candies-to-people/1195-distribute-candies-to-people.py
+++ 1195-distribute-candies-to-people/1195-distribute-candies-to-people.py
@@ -1,31 +1,10 @@
 class Solution:
     def distributeCandies(self, candies: int, num_people: int) -> List[int]:
-        # ans = [0] * num_people
-
-        # l, i = 0, 1
-        # while candies > 0:
-        #     if candies < i:
-        #         ans[l] += candies
-        #     else:
-        #         ans[l] += i
-        #     candies -= i
-        #     l += 1
-        #     i += 1
-        #     if l >= num_people:
-        #         l = 0
-            
-        # return ans
-
-        res = [0] * num_people
-        count = 0
-        while candies > 0:
-            for i in range(num_people):
-                cur = count * num_people + (i+1)
-                if cur < candies:
-                    res[i] += cur
-                    candies -=  cur
-                else:
-                    res[i] += candies
-                    return res
-            count += 1
-        return res
+        # https://github.com/doocs/leetcode/tree/main/solution/1100-1199/1103.Distribute%20Candies%20to%20People
+        ans = [0] * num_people
+        i = 0
+        while candies:
+            ans[i % num_people] += min(candies, i + 1)
+            candies -= min(candies, i + 1)
+            i += 1
+        return ans
