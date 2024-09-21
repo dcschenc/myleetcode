38446016d534804c125993a1745a1ab508c3978f
dcschenc <dcschenc@gmail.com>
2024-09-21 10:59:26 +0800

    Time: 31 ms (85%), Space: 16.6 MB (50.9%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,23 +1,17 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        def is_divide(str, substr):
-            if len(str)%len(substr) !=0:
+        def is_divide(s, substr):
+            if len(s)%len(substr) !=0:
                 return False
-            m = int(len(str)/len(substr))            
-            return substr * m == str
-                
+            m = int(len(s)/len(substr))            
+            return substr * m == s                
 
         if len(str1) < len(str2):
             str1, str2 = str2, str1
         m, n = len(str1), len(str2)
         while n > 0:
             if is_divide(str1, str2[:n]) and is_divide(str2, str2[:n]):
-                return str2[:n]
-           
-            # if n > len(str2)/2:
-            #     n = len(str2)//2
-            # else:
+                return str2[:n]           
             n -= 1
-
         return ''
 
