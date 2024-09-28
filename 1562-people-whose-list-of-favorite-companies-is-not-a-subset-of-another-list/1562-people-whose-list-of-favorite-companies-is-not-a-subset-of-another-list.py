b4de50e83d9f8e90a69f56c399688e9a4780aaf9
dcschenc <dcschenc@gmail.com>
2024-09-28 18:50:42 +0800

    Time: 444 ms (59.41%), Space: 34.7 MB (50.5%) - LeetHub

--- 1562-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/1562-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.py
+++ 1562-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/1562-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.py
@@ -1,11 +1,5 @@
 class Solution:
-    def peopleIndexes(self, favoriteCompanies: List[List[str]]) -> List[int]:
-        # def isSubset(l1, l2):
-        #     for w in l1:
-        #         if w not in l2:
-        #             return False
-        #     return True
-        
+    def peopleIndexes(self, favoriteCompanies: List[List[str]]) -> List[int]:        
         # O(n^2 * k)
         favorites = [set(x) for x in favoriteCompanies]
         ans = []
@@ -14,52 +8,9 @@ class Solution:
             for j in range(len(favorites)):
                 if i == j or len(favorites[i]) > len(favorites[j]):
                     continue
-                # if isSubset(favorites[i], favorites[j]):
                 if favorites[i].issubset(favorites[j]):
                     found = True
                     break
             if found is False:
                 ans.append(i)
         return ans
-
-        # d = {}
-        # idx = 0
-        # t = []
-        # for v in favoriteCompanies:
-        #     for c in v:
-        #         if c not in d:
-        #             d[c] = idx
-        #             idx += 1
-        #     t.append({d[c] for c in v})
-        # ans = []
-        # for i, nums1 in enumerate(t):
-        #     ok = True
-        #     for j, nums2 in enumerate(t):
-        #         if i == j:
-        #             continue
-        #         if not (nums1 - nums2):
-        #             ok = False
-        #             break
-        #     if ok:
-        #         ans.append(i)
-        # return ans        d = {}
-        # idx = 0
-        # t = []
-        # for v in favoriteCompanies:
-        #     for c in v:
-        #         if c not in d:
-        #             d[c] = idx
-        #             idx += 1
-        #     t.append({d[c] for c in v})
-        # ans = []
-        # for i, nums1 in enumerate(t):
-        #     ok = True
-        #     for j, nums2 in enumerate(t):
-        #         if i == j:
-        #             continue
-        #         if not (nums1 - nums2):
-        #             ok = False
-        #             break
-        #     if ok:
-        #         ans.append(i)
-        # return ans
