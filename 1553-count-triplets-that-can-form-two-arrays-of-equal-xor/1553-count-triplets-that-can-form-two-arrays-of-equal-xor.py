04f6c63417b4ad9bedad08c83f5b78ae9291c707
dcschenc <dcschenc@gmail.com>
2024-09-28 15:03:22 +0800

    Time: 38 ms (91.15%), Space: 16.6 MB (49.34%) - LeetHub

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -1,35 +1,18 @@
 class Solution:
     def countTriplets(self, arr: List[int]) -> int:
-        # https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/
+        # https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/editorial/
         prefix_XOR = [0] + arr[:]
-        size = len(prefix_XOR)
+        n = len(prefix_XOR)
 
         # Perform XOR on consecutive elements in the modified array
-        for i in range(1, size):
+        for i in range(1, n):
             prefix_XOR[i] ^= prefix_XOR[i - 1]
 
         count = 0
 
         # Iterate through the modified array to count triplets
-        for start in range(size):
-            for end in range(start + 1, size):
+        for start in range(n):
+            for end in range(start + 1, n):
                 if prefix_XOR[start] == prefix_XOR[end]:
-                    # Increment the result by the count of valid triplets
                     count += end - start - 1
-
         return count
-
-        n, ans = len(arr), 0
-        for i in range(n-1):
-            a = arr[i]
-            for j in range(i+1, n):
-                if j != i + 1:
-                    a = a ^ arr[j-1]
-                b = arr[j]
-                if a == b: 
-                    ans += 1
-                for k in range(j+1, n):
-                    b = b ^ arr[k]
-                    if a == b:
-                        ans += 1
-        return ans
