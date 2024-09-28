f1489cc3b74ef39fe25f2eb4df60073d315c07a4
dcschenc <dcschenc@gmail.com>
2024-09-28 10:19:45 +0800

    Time: 173 ms (79.54%), Space: 17.8 MB (76.74%) - LeetHub

--- 1531-number-of-ways-to-wear-different-hats-to-each-other/1531-number-of-ways-to-wear-different-hats-to-each-other.py
+++ 1531-number-of-ways-to-wear-different-hats-to-each-other/1531-number-of-ways-to-wear-different-hats-to-each-other.py
@@ -1,6 +1,6 @@
 class Solution:
     def numberWays(self, hats: List[List[int]]) -> int:
-        # https://github.com/doocs/leetcode/tree/main/solution/1400-1499/1434.Number%20of%20Ways%20to%20Wear%20Different%20Hats%20to%20Each%20Other
+        # # https://github.com/doocs/leetcode/tree/main/solution/1400-1499/1434.Number%20of%20Ways%20to%20Wear%20Different%20Hats%20to%20Each%20Other
         g = defaultdict(list)
         for i, h in enumerate(hats):
             for v in h:
@@ -18,47 +18,28 @@ class Solution:
                         f[i][j] = (f[i][j] + f[i - 1][j ^ (1 << k)]) % mod
         return f[m][-1]
         
-        mod = 10**9 + 7
-        n = len(hats)
+        # mod = 10**9 + 7
+        # n = len(hats)
 
-        # Create a list of people who like each hat
-        hat_to_people = defaultdict(list)
-        for person, hat_list in enumerate(hats):
-            for hat in hat_list:
-                hat_to_people[hat].append(person)
+        # # Create a list of people who like each hat
+        # hat_to_people = defaultdict(list)
+        # for person, hat_list in enumerate(hats):
+        #     for hat in hat_list:
+        #         hat_to_people[hat].append(person)
 
-        # dp[mask] will be the number of ways to assign hats to people represented by mask
-        dp = [0] * (1 << n)
-        dp[0] = 1
+        # # dp[mask] will be the number of ways to assign hats to people represented by mask
+        # dp = [0] * (1 << n)
+        # dp[0] = 1
 
-        for hat in range(1, 41):
-            if hat not in hat_to_people:
-                continue
+        # for hat in range(1, 41):
+        #     if hat not in hat_to_people:
+        #         continue
 
-            # Traverse the dp array from right to left to avoid using the same hat in the same iteration
-            for mask in range((1 << n) - 1, -1, -1):
-                for person in hat_to_people[hat]:
-                    if mask & (1 << person) == 0:
-                        dp[mask | (1 << person)] = (dp[mask | (1 << person)] + dp[mask]) % mod
+        #     # Traverse the dp array from right to left to avoid using the same hat in the same iteration
+        #     for mask in range((1 << n) - 1, -1, -1):
+        #         for person in hat_to_people[hat]:
+        #             if mask & (1 << person) == 0:
+        #                 dp[mask | (1 << person)] = (dp[mask | (1 << person)] + dp[mask]) % mod
 
-        return dp[(1 << n) - 1]
+        # return dp[(1 << n) - 1]
 
-
-        def backtrack(i):
-            nonlocal ans 
-            if i == n:                
-                return 1
-            total = 0    
-            for h in hats[i]:
-                if h not in visited:
-                    visited.append(h)
-                    total += backtrack(i + 1)
-                    total = total % mod
-                    visited.pop()              
-            return total      
-
-        visited = []        
-        n = len(hats)
-        ans, mod = 0, 10 ** 9 + 7
-        ans = backtrack(0)
-        return ans % mod
