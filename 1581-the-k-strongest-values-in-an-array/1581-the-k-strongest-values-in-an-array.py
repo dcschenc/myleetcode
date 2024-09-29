ffa671c5ac5d83f1514921d1760396e8ef656427
dcschenc <dcschenc@gmail.com>
2024-09-29 10:14:26 +0800

    Time: 715 ms (69.54%), Space: 34.6 MB (47.68%) - LeetHub

--- 1581-the-k-strongest-values-in-an-array/1581-the-k-strongest-values-in-an-array.py
+++ 1581-the-k-strongest-values-in-an-array/1581-the-k-strongest-values-in-an-array.py
@@ -2,27 +2,27 @@ class Solution:
     def getStrongest(self, arr: List[int], k: int) -> List[int]:
         arr.sort()
         m = arr[(len(arr) - 1) >> 1]
-        arr.sort(key=lambda x: (-abs(x - m), -x))
+        arr.sort(key=lambda x: (abs(x - m), x), reverse=True)
         return arr[:k]
 
-        arr.sort()
-        n = len(arr)
-        m = arr[(n-1)//2]
-        i, j = 0, len(arr)-1
-        ans = []
-        while i <= j:            
-            diff = abs(arr[i] - m) - abs(arr[j] - m)
-            if diff > 0:                
-                ans.append(arr[i])
-                i += 1
-            elif diff == 0:
-                if arr[i] > arr[j]:                    
-                    ans.append(arr[i])
-                    i += 1
-                elif arr[i] <= arr[j]:                    
-                    ans.append(arr[j])
-                    j -= 1
-            else:
-                ans.append(arr[j])
-                j -= 1
-        return ans[:k]
+        # arr.sort()
+        # n = len(arr)
+        # m = arr[(n-1)//2]
+        # i, j = 0, len(arr)-1
+        # ans = []
+        # while i <= j:            
+        #     diff = abs(arr[i] - m) - abs(arr[j] - m)
+        #     if diff > 0:                
+        #         ans.append(arr[i])
+        #         i += 1
+        #     elif diff == 0:
+        #         if arr[i] > arr[j]:                    
+        #             ans.append(arr[i])
+        #             i += 1
+        #         elif arr[i] <= arr[j]:                    
+        #             ans.append(arr[j])
+        #             j -= 1
+        #     else:
+        #         ans.append(arr[j])
+        #         j -= 1
+        # return ans[:k]
