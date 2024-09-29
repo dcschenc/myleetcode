d1337bef396627a551dc6f810c677fecd314b5a3
dcschenc <dcschenc@gmail.com>
2024-09-29 14:38:07 +0800

    Time: 864 ms (19.32%), Space: 34.7 MB (37.2%) - LeetHub

--- 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.py
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.py
@@ -23,20 +23,20 @@ class Solution:
         
         return result
 
-        n = len(rains)
-        ans = [-1] * n
-        sunny = SortedList()
-        rainy = {}
-        for i, v in enumerate(rains):
-            if v:
-                if v in rainy:
-                    idx = sunny.bisect_right(rainy[v])
-                    if idx == len(sunny):
-                        return []
-                    ans[sunny[idx]] = v
-                    sunny.discard(sunny[idx])
-                rainy[v] = i
-            else:
-                sunny.add(i)
-                ans[i] = 1
-        return ans
+        # n = len(rains)
+        # ans = [-1] * n
+        # sunny = SortedList()
+        # rainy = {}
+        # for i, v in enumerate(rains):
+        #     if v:
+        #         if v in rainy:
+        #             idx = sunny.bisect_right(rainy[v])
+        #             if idx == len(sunny):
+        #                 return []
+        #             ans[sunny[idx]] = v
+        #             sunny.discard(sunny[idx])
+        #         rainy[v] = i
+        #     else:
+        #         sunny.add(i)
+        #         ans[i] = 1
+        # return ans
