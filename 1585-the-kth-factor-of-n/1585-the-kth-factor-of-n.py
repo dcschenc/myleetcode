4f3ffeb1b17fc3fee863611c7b79e5c45a54cdb3
dcschenc <dcschenc@gmail.com>
2024-09-29 14:55:11 +0800

    Time: 39 ms (35.49%), Space: 16.6 MB (52.36%) - LeetHub

--- 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.py
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.py
@@ -1,7 +1,7 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
         cnt = 0
-        for i in range(1, n+1):
+        for i in range(1, n + 1):
             if n % i == 0:
                 cnt += 1
             if cnt == k:
