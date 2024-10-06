c10b536109d2838ed6b0dc7b1b6f1e0a878f86dc
dcschenc <dcschenc@gmail.com>
2024-10-06 13:54:39 +0800

    Time: 1638 ms (26.72%), Space: 61.8 MB (20.08%) - LeetHub

--- 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.py
@@ -1,6 +1,15 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
         # https://github.com/doocs/leetcode/tree/main/solution/1800-1899/1899.Merge%20Triplets%20to%20Form%20Target%20Triplet
+        x, y, z = target
+        d = e = f = 0
+        for a, b, c in triplets:
+            if a <= x and b <= y and c <= z:
+                d = max(d, a)
+                e = max(e, b)
+                f = max(f, c)
+        return [d, e, f] == target
+        
         a, b, c = [], [], []
         for x, y, z in triplets:
             if x == target[0] and y <= target[1] and z <= target[2]:
