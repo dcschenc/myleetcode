d00e685445ca6bb77ea5f8e1354300c288810d3d
dcschenc <dcschenc@gmail.com>
2024-10-11 20:41:06 +0800

    Time: 430 ms (34.38%), Space: 210.1 MB (5.47%) - LeetHub

--- 2251-number-of-ways-to-divide-a-long-corridor/2251-number-of-ways-to-divide-a-long-corridor.py
+++ 2251-number-of-ways-to-divide-a-long-corridor/2251-number-of-ways-to-divide-a-long-corridor.py
@@ -1,22 +1,33 @@
 class Solution:
     def numberOfWays(self, corridor: str) -> int:
         # https://github.com/doocs/leetcode/tree/main/solution/2100-2199/2147.Number%20of%20Ways%20to%20Divide%20a%20Long%20Corridor
-        @cache
-        def dfs(i, cnt):
-            if i == n:
-                return int(cnt == 2)
-            if corridor[i] == 'S':
-                cnt += 1
-            if cnt > 2:
-                return 0
-            ans = dfs(i + 1, cnt)
-            if cnt == 2:
-                ans += dfs(i + 1, 0)
-                ans %= mod
-            return ans
-
+        MOD = 10**9 + 7
         n = len(corridor)
-        mod = 10**9 + 7
-        ans = dfs(0, 0)
-        dfs.cache_clear()
-        return ans        
+        seat_count = corridor.count('S')
+        
+        # If there aren't exactly 2n seats, it's impossible to partition as required
+        if seat_count < 2 or seat_count % 2 != 0:
+            return 0
+
+        # List to store the indices of all seats in the corridor
+        seats = [i for i, c in enumerate(corridor) if c == 'S']
+        
+        # DFS function with memoization
+        memo = {}
+        def dfs(index):
+            # If we've reached the last pair of seats, there's only one way to end
+            if index == len(seats) - 1:
+                return 1
+            
+            if index in memo:
+                return memo[index]
+            
+            # Find the gap between this seat and the next
+            gap = seats[index + 1] - seats[index]
+            
+            # Calculate the number of ways recursively
+            memo[index] = (gap * dfs(index + 2)) % MOD
+            return memo[index]
+        
+        # Start DFS from the first seat pair
+        return dfs(1)
