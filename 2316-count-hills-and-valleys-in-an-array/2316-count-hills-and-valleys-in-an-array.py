c248f97bd5f064e63060f1dd8b4074a5cbe39e24
dcschenc <dcschenc@gmail.com>
2024-10-13 09:34:33 +0800

    Time: 38 ms (75.71%), Space: 16.6 MB (8.38%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
@@ -1,5 +1,6 @@
 class Solution:
     def countHillValley(self, nums: List[int]) -> int:
+        # https://github.com/doocs/leetcode/tree/main/solution/2200-2299/2210.Count%20Hills%20and%20Valleys%20in%20an%20Array
         ans = j = 0
         for i in range(1, len(nums) - 1):
             if nums[i] == nums[i + 1]:
@@ -10,45 +11,3 @@ class Solution:
                 ans += 1
             j = i
         return ans
-
-        # arr = [nums[0]]
-        # for v in nums[1:]:
-        #     if v != arr[-1]:
-        #         arr.append(v)
-        # return sum(
-        #     (arr[i] < arr[i - 1]) == (arr[i] < arr[i + 1])
-        #     for i in range(1, len(arr) - 1)
-        # )
-        
-        # cnt = 0
-        # i, n = 1, len(nums)
-        # hill_valleys = [0] * n
-        # while i < n - 1:
-        #     l = i - 1
-        #     while l >= 0 and nums[l] == nums[i]:
-        #         l -= 1
-        #     hill, valley = 0, 0
-        #     if l >= 0:
-        #         if nums[i] > nums[l]:
-        #             hill += 1
-        #         else:
-        #             valley += 1
-        #     r = i + 1
-        #     while r <= n-1 and nums[r] == nums[i]:
-        #         r += 1
-        #     if r <= n-1:
-        #         if nums[i] > nums[r]:
-        #             hill += 1
-        #         else:
-        #             valley += 1
-        #     if hill == 2:
-        #         hill_valleys[i] = 'h'
-        #     if valley == 2:
-        #         hill_valleys[i] = 'v'
-        #     i += 1
-        
-        # ans = 0
-        # for i, c in enumerate(hill_valleys):
-        #     if (c == 'h' or c == 'v') and hill_valleys[i-1] != c:
-        #         ans += 1
-        # return ans
