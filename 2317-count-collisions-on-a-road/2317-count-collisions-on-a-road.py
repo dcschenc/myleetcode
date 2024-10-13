f633958e98d3313eb3a0875e328c833b7ae601c4
dcschenc <dcschenc@gmail.com>
2024-10-13 09:39:21 +0800

    Time: 44 ms (99.07%), Space: 17.6 MB (68.85%) - LeetHub

--- 2317-count-collisions-on-a-road/2317-count-collisions-on-a-road.py
+++ 2317-count-collisions-on-a-road/2317-count-collisions-on-a-road.py
@@ -10,22 +10,5 @@ class Solution:
         # collisions = len(sanitized_directions) - sanitized_directions.count('S')
 
         # return collisions
-
-        stack = []
-        collisions = 0
-        for d in directions:
-            if d == 'R':
-                stack.append('R')
-                continue
-            if d == 'L':
-                if not stack:
-                    continue
-                collisions += 2 if stack[-1] == 'R' else 1
-                stack.pop()
-                d = 'S'
-            if d == 'S':
-                while stack and stack[-1] == 'R':
-                    stack.pop()
-                    collisions += 1
-                stack.append('S')
-        return collisions
+        d = directions.lstrip('L').rstrip('R')
+        return len(d) - d.count('S')
