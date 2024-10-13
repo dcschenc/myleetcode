715a9c0537d8871ebea83b2551a588da187a4b10
dcschenc <dcschenc@gmail.com>
2024-10-13 15:39:41 +0800

    Time: 152 ms (32.94%), Space: 17.5 MB (60.24%) - LeetHub

--- 2340-total-appeal-of-a-string/2340-total-appeal-of-a-string.py
+++ 2340-total-appeal-of-a-string/2340-total-appeal-of-a-string.py
@@ -2,10 +2,14 @@ class Solution:
     def appealSum(self, s: str) -> int:
         # https://github.com/doocs/leetcode/tree/main/solution/2200-2299/2262.Total%20Appeal%20of%20A%20String
         ans = t = 0
-        pos = [-1] * 26
+        # pos = [-1] * 26
+        seen = {}
         for i, c in enumerate(s):
             c = ord(c) - ord('a')
-            t += i - pos[c]
+            if c not in seen:
+                t += i + 1
+            else:
+                t += i - seen[c]
             ans += t
-            pos[c] = i
+            seen[c] = i
         return ans
