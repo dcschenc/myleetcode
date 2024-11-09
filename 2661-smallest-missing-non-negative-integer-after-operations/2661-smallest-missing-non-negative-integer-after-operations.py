f37dd8b062f8d953a1c0a2c9f0923b8d291d3c29
dcschenc <dcschenc@gmail.com>
2024-11-09 09:41:14 +0800

    Time: 189 ms (28.87%), Space: 36.4 MB (29.44%) - LeetHub

--- 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.py
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.py
@@ -2,12 +2,12 @@ class Solution:
     def findSmallestInteger(self, nums: List[int], value: int) -> int:
         # https://algo.monster/liteproblems/2598
         # Create a counter to keep track of the frequency of each number modulo 'value'
-        modulo_counter = Counter(num % value for num in nums)
+        cnt = Counter(num % value for num in nums)
       
         # Iterate over the numbers from 0 to length of nums (inclusive)
         for i in range(len(nums) + 1):
             # If there is no number i modulo 'value' in our collection, return i
-            if modulo_counter[i % value] == 0:
+            if cnt[i % value] == 0:
                 return i
             # Otherwise, decrease the count of i modulo 'value' by one
-            modulo_counter[i % value] -= 1
+            cnt[i % value] -= 1
