1ef090d983dcc69c024540459622871fb2c7efa8
dcschenc <dcschenc@gmail.com>
2024-11-16 14:36:33 +0800

    Time: 395 ms (9.86%), Space: 48.9 MB (5.32%) - LeetHub

--- 3158-maximum-length-of-semi-decreasing-subarrays/3158-maximum-length-of-semi-decreasing-subarrays.py
+++ 3158-maximum-length-of-semi-decreasing-subarrays/3158-maximum-length-of-semi-decreasing-subarrays.py
@@ -1,18 +1,6 @@
 from sortedcontainers import SortedList
 class Solution:
-    def maxSubarrayLength(self, nums: List[int]) -> int:
-        n = len(nums)
-        stack = []
-        for i in range(n):
-            if not stack or nums[i] > nums[stack[-1]]:
-                stack.append(i)
-        ans = 0
-        for i in range(n - 1, -1, -1):
-            while len(stack) > 0 and nums[stack[-1]] > nums[i]:
-                ans = max(ans, i - stack[-1] + 1)
-                stack.pop(-1)
-        return ans
-        
+    def maxSubarrayLength(self, nums: List[int]) -> int:        
         # https://github.com/doocs/leetcode/tree/main/solution/2800-2899/2863.Maximum%20Length%20of%20Semi-Decreasing%20Subarrays
         d = defaultdict(list)
         for i, x in enumerate(nums):
@@ -23,18 +11,18 @@ class Solution:
             k = min(k, d[x][0])
         return ans
 
-        n = len(nums)
-        hm, ans = {}, 0
-        keys = SortedList()
-        for i, num in enumerate(nums):
-            j = n + 1
-            right = bisect_right(keys, x=num)
-            for k in keys[right:]:
-                j = min(hm[k], j)                
-            if j != n + 1:
-                ans = max(ans, i - j + 1)
-            if num not in hm:
-                hm[num] = i
-                keys.add((num, i))
-        return ans
+        # n = len(nums)
+        # hm, ans = {}, 0
+        # keys = SortedList()
+        # for i, num in enumerate(nums):
+        #     j = n + 1
+        #     right = bisect_right(keys, x=num)
+        #     for k in keys[right:]:
+        #         j = min(hm[k], j)                
+        #     if j != n + 1:
+        #         ans = max(ans, i - j + 1)
+        #     if num not in hm:
+        #         hm[num] = i
+        #         keys.add((num, i))
+        # return ans
             
